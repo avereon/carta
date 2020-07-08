d0f07ff633c2d36b74d6b5d0a9a62085ec85f5d3
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-07-07 18:00:33 -0600

    Register icons

--- source/main/java/com/avereon/cart2/Cartesia2dMod.java
+++ source/main/java/com/avereon/cart2/Cartesia2dMod.java
@@ -2,4 +2,18 @@ package com.avereon.cart2;
 
 import com.avereon.xenon.Mod;
 
-public class Cartesia2dMod extends Mod {}
+public class Cartesia2dMod extends Mod {
+
+	@Override
+	public void startup() {
+		super.startup();
+		registerIcon( "cart2", new Cartesia2dIcon() );
+	}
+
+	@Override
+	public void shutdown() {
+		unregisterIcon( "cart2", new Cartesia2dIcon() );
+		super.shutdown();
+	}
+
+}
