73b068a2fff66193e9151062ade733be45978dec
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-07-09 15:42:16 -0600

    Update mod icon

--- source/main/java/com/avereon/cart2/Cartesia2dIcon.java
+++ source/main/java/com/avereon/cart2/Cartesia2dIcon.java
@@ -19,12 +19,9 @@ public class Cartesia2dIcon extends SvgIcon {
 
 	public Cartesia2dIcon() {
 		super( 32, 32 );
-		//super( 24, 24, "M16.24,11.51l1.57-1.57l-3.75-3.75l-1.57,1.57L8.35,3.63 c-0.78-0.78-2.05-0.78-2.83,0l-1.9,1.9 c-0.78,0.78-0.78,2.05,0,2.83l4.13,4.13L3,17.25V21h3.75l4.76-4.76l4.13,4.13c0.95,0.95,2.23,0.6,2.83,0l1.9-1.9 c0.78-0.78,0.78-2.05,0-2.83L16.24,11.51z M9.18,11.07L5.04,6.94l1.89-1.9c0,0,0,0,0,0l1.27,1.27L7.02,7.5l1.41,1.41l1.19-1.19 l1.45,1.45L9.18,11.07z M17.06,18.96l-4.13-4.13l1.9-1.9l1.45,1.45l-1.19,1.19l1.41,1.41l1.19-1.19l1.27,1.27L17.06,18.96z M20.71,7.04c0.39-0.39,0.39-1.02,0-1.41l-2.34-2.34c-0.47-0.47-1.12-0.29-1.41,0l-1.83,1.83l3.75,3.75L20.71,7.04z" );
-
-		double halfSize = 0.5 * KNOB;
-		double a = Math.sqrt( RR * RR - halfSize * halfSize );
 
 		double halfKnob = 0.5 * KNOB;
+		double a = Math.sqrt( RR * RR - halfKnob * halfKnob );
 
 		String top = "";
 		top += "M" + (CX - halfKnob) + "," + (CY - RR - KNOB);
@@ -39,33 +36,40 @@ public class Cartesia2dIcon extends SvgIcon {
 		double alpha = Math.atan2( 1, 2 );
 		// 270 deg - alpha
 		double beta = 1.5 * Math.PI - alpha;
-		double aX = SS * -Math.cos( beta );
-		double aY = SS * -Math.sin( beta );
-		double offsetX = 10;
-		double offsetY = 2 * offsetX;
-
 		// Perpendicular to beta (180 deg - alpha)
 		double gamma = beta - 0.5 * Math.PI;
+
 		double d = Math.sqrt( SS * SS - ARM_WIDTH * ARM_WIDTH );
-		double dX = CX + d * Math.cos( beta );
-		double dY = CY - d * Math.sin( beta );
-		double eX = dX + ARM_WIDTH * Math.cos( gamma );
-		double eY = dY - ARM_WIDTH * Math.sin( gamma );
 
-		double hX = CX - eX;
-		//double gX = CX - aX;
+		// The top-left point of the arm
+		double aX = -d * Math.cos( beta ) - ARM_WIDTH * Math.cos( gamma );
+		double aY = -d * Math.sin( beta ) - ARM_WIDTH * Math.sin( gamma );
 
-		String left = "M" + (CX - hX) + "," + eY;
-		left += "A" + SS + "," + SS + " 0 0 0 " + (CX - aX) + "," + (CY + aY);
-		left += "L" + (CX - offsetX) + "," + (CY + offsetY);
-		left += "l0," + -ARM_WIDTH / Math.sin( alpha );
+		// The top-right point of the arm
+		double bX = SS * -Math.cos( beta );
+		double bY = SS * -Math.sin( beta );
+
+		// The bottom-right point of the arm (the offset from center to the arm tip)
+		double cX = 10;
+		double cY = 2 * cX;
+
+		// The bottom-left (even thought it is directly above point c) point of the arm
+		double dX = cX + 0;
+		double dY = cY - (ARM_WIDTH / Math.sin( alpha ));
+
+		// The left arm
+		String left = "M" + (CX - aX) + "," + (CY + aY);
+		left += "A" + SS + "," + SS + " 0 0 0 " + (CX - bX) + "," + (CY + bY);
+		left += "L" + (CX - cX) + "," + (CY + cY);
+		left += "L" + (CX - dX) + "," + (CY + dY);
 		left += "Z";
 		add( left );
 
-		String right = "M" + (CX + hX) + "," + eY;
-		right += "A" + SS + "," + SS + " 0 0 1 " + (CX + aX) + "," + (CY + aY);
-		right += "L" + (CX + offsetX) + "," + (CY + offsetY);
-		right += "l0," + -ARM_WIDTH / Math.sin( alpha );
+		// Mirror the left arm
+		String right = "M" + (CX + aX) + "," + (CY + aY);
+		right += "A" + SS + "," + SS + " 0 0 1 " + (CX + bX) + "," + (CY + bY);
+		right += "L" + (CX + cX) + "," + (CY + cY);
+		right += "L" + (CX + dX) + "," + (CY + dY);
 		right += "Z";
 		add( right );
 	}
