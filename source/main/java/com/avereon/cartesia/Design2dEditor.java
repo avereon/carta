e162e7402a930171752034f1714b3e83f836799d
Mark Soderquist <mark@soderquist.net>
2020-07-11 19:58:21 -0600

    Create asset type and initial tools

--- source/main/java/com/avereon/cartesia/Design2dEditor.java
+++ source/main/java/com/avereon/cartesia/Design2dEditor.java
@@ -1,11 +1,17 @@
 package com.avereon.cartesia;
 
+import com.avereon.util.Log;
+import com.avereon.venza.image.VectorImage;
 import com.avereon.xenon.ProgramProduct;
 import com.avereon.xenon.ProgramTool;
 import com.avereon.xenon.asset.Asset;
+import javafx.geometry.Dimension2D;
+import javafx.scene.ImageCursor;
 import javafx.scene.layout.Pane;
 
 public class Design2dEditor extends ProgramTool {
+
+	private static final System.Logger log = Log.get();
 
 	private final Pane geometry;
 
@@ -13,6 +19,12 @@ public class Design2dEditor extends ProgramTool {
 		super( product, asset );
 
 		getChildren().add( geometry = new Pane() );
+
+		// NEXT Create an icon for the crosshair cursor
+		VectorImage icon = (VectorImage)getProgram().getIconLibrary().getIcon( "document" );
+
+		Dimension2D size = ImageCursor.getBestSize( 48, 48 );
+		setCursor( new ImageCursor( icon.resize( size.getWidth() ).getImage() ) );
 
 		//onMousePressedProperty().set( e -> add( new Circle(100,100,100, Color.RED) ) );
 
