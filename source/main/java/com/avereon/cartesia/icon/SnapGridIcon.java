8e457a86661e3eb618ae478ed6f6e32e1d277251
Mark Soderquist <mark@soderquist.net>
2020-11-22 17:53:08 -0700

    Update grid icons

--- source/main/java/com/avereon/cartesia/icon/SnapGridIcon.java
+++ source/main/java/com/avereon/cartesia/icon/SnapGridIcon.java
@@ -15,20 +15,14 @@ public class SnapGridIcon extends SvgIcon implements GridIconConstants {
 	public SnapGridIcon( boolean enabled ) {
 		super( GRID, GRID );
 		double offset = C;
-		//for( int index = 0; index < 3; index++ ) {
-			//double offset = INDENT + SPACING * index;
-			if( enabled ) {
-				draw( "M" + offset + " " + MIN + " L" + offset + " " + MAX, 2 );
-				draw( "M" + MIN + " " + offset + " L" + MAX + " " + offset, 2 );
-			} else {
-				draw( "M" + offset + " " + MIN + " L" + offset + " " + MAX, null, 2, StrokeLineCap.ROUND, StrokeLineJoin.MITER, 0.5, 1, 4 );
-				draw( "M" + MIN + " " + offset + " L" + MAX + " " + offset, null, 2, StrokeLineCap.ROUND, StrokeLineJoin.MITER, 0.5, 1, 4 );
-			}
-		//}
 		if( enabled ) {
+			draw( "M" + offset + " " + MIN + " L" + offset + " " + MAX, 2 );
+			draw( "M" + MIN + " " + offset + " L" + MAX + " " + offset, 2 );
 			fill( circle( C, C, RADIUS ) );
 		} else {
-			draw( circle( C, C, RADIUS + 2 ), 1 );
+			draw( "M" + offset + " " + MIN + " L" + offset + " " + MAX, null, 2, StrokeLineCap.ROUND, StrokeLineJoin.MITER, 0.5, 1, 4 );
+			draw( "M" + MIN + " " + offset + " L" + MAX + " " + offset, null, 2, StrokeLineCap.ROUND, StrokeLineJoin.MITER, 0.5, 1, 4 );
+			draw( circle( C, C, RADIUS + 1 ), null, 2, StrokeLineCap.ROUND, StrokeLineJoin.MITER, 0.125 * Math.PI, 0.25 * Math.PI, 1 * Math.PI );
 		}
 	}
 
