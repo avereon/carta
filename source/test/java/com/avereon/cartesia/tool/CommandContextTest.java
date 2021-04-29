e04c56c0fe0c9c10e3c76362f9636def9857b43a
msoderquist <msoderquist@mastercontrol.com>
2021-04-29 10:54:32 -0600

    Fix unit tests

--- source/test/java/com/avereon/cartesia/tool/CommandContextTest.java
+++ source/test/java/com/avereon/cartesia/tool/CommandContextTest.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CommandContextTest {
@@ -24,6 +25,9 @@ public class CommandContextTest {
 	public void setup() throws Exception {
 		this.product = new MockCartesiaMod();
 		this.context = new CommandContext( product );
+
+		assertNotNull( product );
+		assertNotNull( context );
 	}
 
 	@Test
