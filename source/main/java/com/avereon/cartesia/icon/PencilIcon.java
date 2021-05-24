6c2acc30a4aeee9de00a651d6573e9cd2eb0c993
msoderquist <msoderquist@mastercontrol.com>
2021-05-24 12:54:32 -0600

    Update icons

--- source/main/java/com/avereon/cartesia/icon/PencilIcon.java
+++ source/main/java/com/avereon/cartesia/icon/PencilIcon.java
@@ -17,8 +17,8 @@ public class PencilIcon extends SvgIcon {
 	protected void doRender() {
 		fill( "M3,29L5,25L7,27Z" );
 		fill( "M24,4L28,8L26,10L22,6Z", Colors.mix( (Color)getStrokePaint(), Color.TRANSPARENT, 0.2 ) );
-		fill( "M24,4L25,3A2,1,45,0,1,29,7L28,8Z", Color.HOTPINK );
-		draw( "M3,29L7,21L25,3A2,1,45,0,1,29,7L11,25Z M24,4L28,8 M22,6L26,10 M7,21L7,23L9,23L9,25L11,25", null, 1, StrokeLineCap.BUTT, StrokeLineJoin.MITER );
+		fill( "M24,4L25,3A2,1,45,0,1,29,7L28,8Z", Colors.mix( Color.HOTPINK, Color.TRANSPARENT, 0.2 ) );
+		draw( "M3,29L7,21L25,3A2,1,45,0,1,29,7L11,25Z M9,23L24,8 M24,4L28,8 M22,6L26,10 M7,21L7,22L9,23L10,25L11,25", null, 1, StrokeLineCap.BUTT, StrokeLineJoin.MITER );
 		super.doRender();
 	}
 
