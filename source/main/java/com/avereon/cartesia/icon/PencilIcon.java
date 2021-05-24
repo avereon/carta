73180cd1dd22bc97bf76f58d4ab02f04eb45c2d9
Mark Soderquist <mark@soderquist.net>
2021-05-24 09:03:19 -0600

    Continue work on toolbar trays

---
+++ source/main/java/com/avereon/cartesia/icon/PencilIcon.java
@@ -0,0 +1,11 @@
+package com.avereon.cartesia.icon;
+
+import com.avereon.zerra.image.SvgIcon;
+
+public class PencilIcon extends SvgIcon {
+
+	public PencilIcon() {
+		super( 32, 32, "" );
+	}
+
+}
