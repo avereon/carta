37d0034cde2219be6a47dd32d176caf6c8505140
msoderquist <msoderquist@mastercontrol.com>
2022-11-22 14:16:24 -0700

    Fix loading problems with product and program schemes

--- source/main/java/com/avereon/cartesia/CartesiaScheme.java
+++ source/main/java/com/avereon/cartesia/CartesiaScheme.java
@@ -1,6 +1,8 @@
 package com.avereon.cartesia;
 
 import com.avereon.xenon.Program;
+import com.avereon.xenon.asset.Asset;
+import com.avereon.xenon.asset.AssetException;
 import com.avereon.xenon.scheme.ProductScheme;
 
 public class CartesiaScheme extends ProductScheme {
@@ -9,6 +11,11 @@ public class CartesiaScheme extends ProductScheme {
 
 	public CartesiaScheme( Program program ) {
 		super( program, ID );
+	}
+
+	@Override
+	public boolean canLoad( Asset asset ) throws AssetException {
+		return true;
 	}
 
 }
