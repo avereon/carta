c8773bedbd7077baa3fe3dbcc4e32bd4f6dfe506
msoderquist <msoderquist@mastercontrol.com>
2023-01-30 14:07:53 -0700

    Cleanup

--- source/main/java/com/avereon/cartesia/CartesiaMod.java
+++ source/main/java/com/avereon/cartesia/CartesiaMod.java
@@ -41,74 +41,26 @@ public class CartesiaMod extends Mod {
 	@Override
 	public void startup() throws Exception {
 		log.atFine().log( "%s starting...", LazyEval.of( () -> getCard().getName() ) );
+
 		super.startup();
-		registerIcon( getCard().getArtifact(), new CartesiaIcon() );
-		registerIcon( "tool", new BrokenIcon() );
-		registerIcon( "draw", new PencilIcon() );
-		registerIcon( "arc-2", new Arc2Icon() );
-		registerIcon( "arc-3", new Arc3Icon() );
-		registerIcon( "circle-2", new Circle2Icon() );
-		registerIcon( "circle-3", new Circle3Icon() );
-		registerIcon( "circle-diameter-2", new CircleDiameter2Icon() );
-		registerIcon( "curve-3", new Curve3Icon() );
-		registerIcon( "curve-4", new Curve4Icon() );
-		registerIcon( "ellipse-3", new Ellipse3Icon() );
-		registerIcon( "ellipse-arc-5", new EllipseArc5Icon() );
-		registerIcon( "line-2", new Line2Icon() );
-		registerIcon( "line-perpendicular", new LinePerpendicularIcon() );
-		registerIcon( "marker", new MarkerIcon() );
-		registerIcon( "path", new PathIcon() );
-		registerIcon( "layer", new LayerVisibleIcon() );
-		registerIcon( "layers", new LayersIcon() );
-		registerIcon( "layer-hidden", new LayerHiddenIcon() );
-		registerIcon( "grid-toggle-enabled", new GridIcon( true ) );
-		registerIcon( "grid-toggle-disabled", new GridIcon( false ) );
-		registerIcon( "snap-grid-toggle-enabled", new SnapGridIcon( true ) );
-		registerIcon( "snap-grid-toggle-disabled", new SnapGridIcon( false ) );
-		registerIcon( "shape-properties", new PreferencesIcon() );
-		registerIcon( "views", new ViewIcon() );
-		registerIcon( "view", new ViewIcon() );
-		registerIcon( "prints", new PrinterIcon() );
-
-		registerAction( this, "tool" );
-		registerAction( this, "draw" );
-		registerAction( this, "marker" );
-		registerAction( this, "line" );
-		registerAction( this, "circle" );
-		//registerAction( this, "arc" );
-		registerAction( this, "ellipse" );
-		//registerAction( this, "ellipse-arc" );
-		registerAction( this, "curve" );
-		registerAction( this, "draw-arc-2" );
-		registerAction( this, "draw-arc-3" );
-		registerAction( this, "draw-circle-2" );
-		registerAction( this, "draw-circle-3" );
-		//registerAction( this, "draw-curve-3" );
-		registerAction( this, "draw-curve-4" );
-		registerAction( this, "draw-ellipse-3" );
-		registerAction( this, "draw-ellipse-arc-5" );
-		registerAction( this, "draw-line-2" );
-		registerAction( this, "draw-line-perpendicular" );
-		registerAction( this, "draw-marker" );
-		registerAction( this, "draw-path" );
-
-		registerAction( this, "snap-grid-toggle" );
-		registerAction( this, "grid-toggle" );
-
-		registerAction( this, "measure" );
+		registerIcons();
+		registerActions();
 
 		getProgram().getAssetManager().addScheme( new CartesiaScheme( getProgram() ) );
 
 		// Register Design2D asset type and tools
 		registerAssetType( design2dAssetType = new Design2dAssetType( this ) );
+		// Settings pages
+		String path = "/" + getClass().getPackageName().replace( ".", "/" );
+		design2dAssetType.setSettingsPages( SettingsPageParser.parse( this, path + "/design/props/design.xml", RbKey.PROPS ) );
+		// Tool registration
 		ToolRegistration design2dEditorRegistration = new ToolRegistration( this, Design2dEditor.class );
 		design2dEditorRegistration.setName( Rb.text( RbKey.LABEL, "design-2d-editor" ) );
 		registerTool( design2dAssetType, design2dEditorRegistration );
-		String path = "/" + getClass().getPackageName().replace( ".", "/" );
-		design2dAssetType.setSettingsPages( SettingsPageParser.parse( this, path + "/design/props/design.xml", RbKey.PROPS ) );
 
 		// Register Design3D asset type and tools
 		//registerAssetType( design3dAssetType = new Design3dAssetType( this ) );
+		// Tool registration
 		//ToolRegistration design3dEditorRegistration = new ToolRegistration( this, Design3dEditor.class );
 		//design3dEditorRegistration.setName( Rb.text(RbKey.LABEL, "design-3d-editor") );
 		//registerTool( design3dAssetType, design3dEditorRegistration );
@@ -162,6 +114,73 @@ public class CartesiaMod extends Mod {
 		unregisterTool( design2dAssetType, Design2dEditor.class );
 		unregisterAssetType( design2dAssetType );
 
+		unregisterActions();
+		unregisterIcons();
+		super.shutdown();
+
+		log.atInfo().log( "%s stopped.", LazyEval.of( () -> getCard().getName() ) );
+	}
+
+	private void registerIcons() {
+		registerIcon( getCard().getArtifact(), new CartesiaIcon() );
+		registerIcon( "tool", new BrokenIcon() );
+		registerIcon( "draw", new PencilIcon() );
+		registerIcon( "arc-2", new Arc2Icon() );
+		registerIcon( "arc-3", new Arc3Icon() );
+		registerIcon( "circle-2", new Circle2Icon() );
+		registerIcon( "circle-3", new Circle3Icon() );
+		registerIcon( "circle-diameter-2", new CircleDiameter2Icon() );
+		registerIcon( "curve-3", new Curve3Icon() );
+		registerIcon( "curve-4", new Curve4Icon() );
+		registerIcon( "ellipse-3", new Ellipse3Icon() );
+		registerIcon( "ellipse-arc-5", new EllipseArc5Icon() );
+		registerIcon( "line-2", new Line2Icon() );
+		registerIcon( "line-perpendicular", new LinePerpendicularIcon() );
+		registerIcon( "marker", new MarkerIcon() );
+		registerIcon( "path", new PathIcon() );
+		registerIcon( "layer", new LayerVisibleIcon() );
+		registerIcon( "layers", new LayersIcon() );
+		registerIcon( "layer-hidden", new LayerHiddenIcon() );
+		registerIcon( "grid-toggle-enabled", new GridIcon( true ) );
+		registerIcon( "grid-toggle-disabled", new GridIcon( false ) );
+		registerIcon( "snap-grid-toggle-enabled", new SnapGridIcon( true ) );
+		registerIcon( "snap-grid-toggle-disabled", new SnapGridIcon( false ) );
+		registerIcon( "shape-properties", new PreferencesIcon() );
+		registerIcon( "views", new ViewIcon() );
+		registerIcon( "view", new ViewIcon() );
+		registerIcon( "prints", new PrinterIcon() );
+	}
+
+	private void registerActions() {
+		registerAction( this, "tool" );
+		registerAction( this, "draw" );
+		registerAction( this, "marker" );
+		registerAction( this, "line" );
+		registerAction( this, "circle" );
+		//registerAction( this, "arc" );
+		registerAction( this, "ellipse" );
+		//registerAction( this, "ellipse-arc" );
+		registerAction( this, "curve" );
+		registerAction( this, "draw-arc-2" );
+		registerAction( this, "draw-arc-3" );
+		registerAction( this, "draw-circle-2" );
+		registerAction( this, "draw-circle-3" );
+		//registerAction( this, "draw-curve-3" );
+		registerAction( this, "draw-curve-4" );
+		registerAction( this, "draw-ellipse-3" );
+		registerAction( this, "draw-ellipse-arc-5" );
+		registerAction( this, "draw-line-2" );
+		registerAction( this, "draw-line-perpendicular" );
+		registerAction( this, "draw-marker" );
+		registerAction( this, "draw-path" );
+
+		registerAction( this, "snap-grid-toggle" );
+		registerAction( this, "grid-toggle" );
+
+		registerAction( this, "measure" );
+	}
+
+	private void unregisterActions() {
 		unregisterAction( "grid-toggle" );
 		unregisterAction( "snap-grid-toggle" );
 
@@ -183,7 +202,9 @@ public class CartesiaMod extends Mod {
 		unregisterAction( "marker" );
 		unregisterAction( "draw" );
 		unregisterAction( "tool" );
+	}
 
+	private void unregisterIcons() {
 		unregisterIcon( "prints", new PrinterIcon() );
 		unregisterIcon( "view", new ViewIcon() );
 		unregisterIcon( "views", new ViewIcon() );
@@ -211,9 +232,6 @@ public class CartesiaMod extends Mod {
 		unregisterIcon( "draw", new PencilIcon() );
 		unregisterIcon( "tool", new BrokenIcon() );
 		unregisterIcon( getCard().getArtifact(), new CartesiaIcon() );
-		super.shutdown();
-
-		log.atInfo().log( "%s stopped.", LazyEval.of( () -> getCard().getName() ) );
 	}
 
 	private void registerHelpPages() {
