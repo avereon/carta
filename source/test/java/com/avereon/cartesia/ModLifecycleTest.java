89c6cceb0ab719f0a0079d9b2e133de908c74d7f
msoderquist <msoderquist@mastercontrol.com>
2024-03-02 17:42:25 -0700

    Rename Mod to Module

--- source/test/java/com/avereon/cartesia/ModLifecycleTest.java
+++ source/test/java/com/avereon/cartesia/ModLifecycleTest.java
@@ -1,6 +1,6 @@
 package com.avereon.cartesia;
 
-import com.avereon.xenon.ModStatus;
+import com.avereon.xenon.Module;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,12 +10,12 @@ public class ModLifecycleTest extends BaseCartesiaUiTest {
 	@Test
 	void testLifecycle() {
 		assertThat( getProgram().getProductManager().isModEnabled( getMod() ) ).isTrue();
-		assertThat( getMod().getStatus() ).isEqualTo( ModStatus.STARTED );
+		assertThat( getMod().getStatus() ).isEqualTo( Module.Status.STARTED );
 
 		getProgram().getProductManager().setModEnabled( getMod().getCard(), false );
 
 		assertThat( getProgram().getProductManager().isModEnabled( getMod() ) ).isFalse();
-		assertThat( getMod().getStatus() ).isEqualTo( ModStatus.STOPPED );
+		assertThat( getMod().getStatus() ).isEqualTo( Module.Status.STOPPED );
 	}
 
 }
