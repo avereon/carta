c224779ba6ff3911dfba1f26c3d6594566b1c575
msoderquist <msoderquist@mastercontrol.com>
2024-03-13 12:54:09 -0600

    Continue work on new design tool

--- source/test/java/com/avereon/cartesia/tool/design/FxRenderDesignToolTest.java
+++ source/test/java/com/avereon/cartesia/tool/design/FxRenderDesignToolTest.java
@@ -8,6 +8,7 @@ import com.avereon.xenon.ProgramToolEvent;
 import com.avereon.xenon.asset.Asset;
 import com.avereon.zarra.event.FxEventWatcher;
 import lombok.CustomLog;
+import lombok.Getter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,43 +19,49 @@ import java.util.concurrent.Future;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+@Getter
 @CustomLog
 public class FxRenderDesignToolTest extends BaseCartesiaUiTest {
 
 	private FxRenderDesignTool tool;
 
-	private Asset asset;
-
-	private Design design;
-
 	@BeforeEach
 	protected void setup() throws Exception {
 		super.setup();
 
+		// Load the design asset into a tool
 		URI uri = Objects.requireNonNull( getClass().getResource( "/design-tool-test.cartesia2d" ) ).toURI();
 		Future<ProgramTool> future = getProgram().getAssetManager().openAsset( uri, FxRenderDesignTool.class );
 		tool = (FxRenderDesignTool)future.get();
-		asset = tool.getAsset();
-		assertNotNull( tool );
-		assertNotNull( asset );
 
 		// Wait for the tool to be ready
 		FxEventWatcher eventWatcher = new FxEventWatcher();
 		tool.addEventHandler( ProgramToolEvent.READY, eventWatcher );
 		eventWatcher.waitForEvent( ProgramToolEvent.READY );
 
-		design = tool.getDesign();
+		// Ensure the test resources are available
+		assertNotNull( getTool() );
+		assertNotNull( getAsset() );
+		assertNotNull( getDesign() );
 	}
 
 	@Test
 	void testAssetTypeResolvedCorrectly() {
-		assertThat( tool.getAsset().getType() ).isInstanceOf( Design2dAssetType.class );
+		assertThat( getAsset().getType() ).isInstanceOf( Design2dAssetType.class );
 	}
 
 	@Test
 	void testVisibleLayers() {
-		assertThat( tool.getDesign().getAllLayers().size() ).isEqualTo( 1 );
-		assertThat( tool.getVisibleLayers().size() ).isEqualTo( 1 );
+		assertThat( getDesign().getAllLayers().size() ).isEqualTo( 1 );
+		assertThat( getTool().getVisibleLayers().size() ).isEqualTo( 1 );
+	}
+
+	protected Asset getAsset() {
+		return tool.getAsset();
+	}
+
+	protected Design getDesign() {
+		return getAsset().getModel();
 	}
 
 }
