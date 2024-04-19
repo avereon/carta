fa430af137977e33fc9f28a61960f6af0e7a27d2
msoderquist <msoderquist@mastercontrol.com>
2024-04-19 13:46:27 -0600

    Continue work on geometry selection

--- source/main/java/com/avereon/cartesia/CommandMap.java
+++ source/main/java/com/avereon/cartesia/CommandMap.java
@@ -178,24 +178,23 @@ public class CommandMap {
 		add( product, "snap-auto-intersection", SnapAuto.class, new SnapIntersection() );
 		//add( product, "snap-auto-grid", SnapAutoCommand.class, new SnapGrid() ); // No one really does this
 
-		// The Ctrl modifier is for panning and zooming (this is roughly how browsers work)
-		// The Shift modifier is for additional selecting
+		// The Shift modifier is for panning and zooming (this is roughly how browsers work)
+		// The Ctrl modifier is for additional selecting
+		// The Ctrl-Shift modifier is for selecting by intersection
 
 		// Single select
 		add( new CommandEventKey( MouseEvent.MOUSE_PRESSED, MouseButton.PRIMARY ), "select" );
 		// Add/remove select
-		// FIXME Add/remove select start event combo is overridden by camera move
 		add( new CommandEventKey( MouseEvent.MOUSE_PRESSED, MouseButton.PRIMARY, true, false, false, false ), "select" );
 		// Snap nearest
 		add( new CommandEventKey( MouseEvent.MOUSE_PRESSED, MouseButton.SECONDARY ), "snap-auto-nearest" );
 		// Camera move (aka pan)
-		// FIXME This overrides the add/remove select command
-		add( new CommandEventKey( MouseEvent.MOUSE_PRESSED, MouseButton.PRIMARY, true, false, false, false ), "camera-move" );
+		add( new CommandEventKey( MouseEvent.MOUSE_PRESSED, MouseButton.PRIMARY, false, true, false, false ), "camera-move" );
 		// Camera spin
-		add( new CommandEventKey( MouseEvent.MOUSE_PRESSED, MouseButton.PRIMARY, true, true, false, false ), "camera-spin" );
+		//add( new CommandEventKey( MouseEvent.MOUSE_PRESSED, MouseButton.PRIMARY, false, true, false, false ), "camera-spin" );
 		// Camera zoom
-		add( new CommandEventKey( ScrollEvent.SCROLL, true, false, false, false ), "camera-zoom" );
-		add( new CommandEventKey( ZoomEvent.ZOOM, true, false, false, false ), "camera-zoom" );
+		add( new CommandEventKey( ScrollEvent.SCROLL, false, true, false, false ), "camera-zoom" );
+		add( new CommandEventKey( ZoomEvent.ZOOM, false, true, false, false ), "camera-zoom" );
 		// Camera walk (moving the camara forward and back)
 		//add( new CommandEventKey( ScrollEvent.SCROLL, false, true, false, false ), "camera-walk" );
 		//add( new CommandEventKey( ZoomEvent.ZOOM, false, true, false, false ), "camera-walk" );
