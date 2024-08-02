1c8ae2b260ba3de15d85c4aed9cb709a319b8378
msoderquist <msoderquist@mastercontrol.com>
2024-08-02 13:25:23 -0600

    Update SelectByPoint command

--- source/test/java/com/avereon/cartesia/CommandBaseTest.java
+++ source/test/java/com/avereon/cartesia/CommandBaseTest.java
@@ -5,19 +5,39 @@ import com.avereon.cartesia.tool.CommandPrompt;
 import com.avereon.cartesia.tool.DesignCommandContext;
 import com.avereon.cartesia.tool.DesignContext;
 import com.avereon.cartesia.tool.DesignTool;
+import com.avereon.xenon.ActionLibrary;
+import com.avereon.xenon.ActionProxy;
+import com.avereon.xenon.Xenon;
+import com.avereon.xenon.XenonProgramProduct;
 import javafx.event.EventType;
 import javafx.scene.input.*;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 
 @ExtendWith( MockitoExtension.class )
 public class CommandBaseTest extends BaseCartesiaUnitTest {
+
+	static XenonProgramProduct product = Mockito.mock( XenonProgramProduct.class );
+
+	static Xenon program = Mockito.mock( Xenon.class );
+
+	static ActionLibrary actionLibrary = Mockito.mock( ActionLibrary.class );
+
+	static ActionProxy other = Mockito.mock( ActionProxy.class );
+
+	static Map<String, ActionProxy> mockActionMap = new HashMap<>();
 
 	@Mock
 	protected DesignTool tool;
@@ -30,6 +50,29 @@ public class CommandBaseTest extends BaseCartesiaUnitTest {
 
 	@Mock
 	protected CommandPrompt commandPrompt;
+
+	@BeforeAll
+	public static void load() {
+		List<String> actions = List.of( "anchor", "select-point", "select-toggle", "select-window-contain", "select-window-intersect", "snap-auto-nearest", "camera-move", "camera-zoom" );
+
+		for( String command : actions ) {
+			ActionProxy action = Mockito.mock( ActionProxy.class );
+			when( action.getName() ).thenReturn( command );
+			mockActionMap.put( command, action );
+		}
+		when( other.getName() ).thenReturn( "other" );
+
+		when( product.getProgram() ).thenReturn( program );
+		when( program.getActionLibrary() ).thenReturn( actionLibrary );
+		when( actionLibrary.getAction( anyString() ) ).thenAnswer( i -> {
+			String name = String.valueOf( i.getArguments()[ 0 ] );
+			return mockActionMap.getOrDefault( name, other );
+		} );
+
+		// With this setup, the CommandMap contain mocks for all the tested actions
+
+		CommandMap.load( product );
+	}
 
 	@BeforeEach
 	protected void setup() {
