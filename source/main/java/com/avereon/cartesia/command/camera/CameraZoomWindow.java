8b5ca93afd070527a3d8bd5fd152453f9db4b35b
msoderquist <msoderquist@mastercontrol.com>
2024-08-07 09:20:51 -0600

    Update CameraZoomWindow command

--- source/main/java/com/avereon/cartesia/command/camera/CameraZoomWindow.java
+++ source/main/java/com/avereon/cartesia/command/camera/CameraZoomWindow.java
@@ -1,6 +1,5 @@
 package com.avereon.cartesia.command.camera;
 
-import com.avereon.cartesia.command.Value;
 import com.avereon.cartesia.tool.BaseDesignTool;
 import com.avereon.cartesia.tool.CommandTask;
 import com.avereon.cartesia.tool.DesignCommandContext;
@@ -47,22 +46,14 @@ public class CameraZoomWindow extends CameraCommand {
 		boolean noEvent = event == null;
 		boolean hasEvent = !noEvent;
 
-		if( paramCount == 0 && noEvent ) {
+		if( paramCount == 0 ) {
 			// Zoom window anchor
-			promptForWindow( task, "zoom-window" );
-			//promptForWindow( task.getContext(), "zoom-window-anchor" );
-			return INCOMPLETE;
-		}
-
-		// If there is an event, but no parameters, use the world anchor as the first parameter
-		if( paramCount == 0 & hasEvent && event.getEventType() == MouseEvent.DRAG_DETECTED ) {
-			// Submit a Value command to pass the anchor back to this command
-			task.getContext().submit( task.getTool(), new Value(), task.getContext().getWorldAnchor() );
+			promptForWindow( task, "zoom-window-anchor" );
 			return INCOMPLETE;
 		}
 
 		// Get the world anchor point from the first parameter
-		if( paramCount == 1 & noEvent ) {
+		if( paramCount == 1 ) {
 			Point3D worldPoint = asPoint( task, task.getParameter( 0 ) );
 			if( worldPoint != null ) {
 				task.getContext().setScreenAnchor( task.getTool().worldToScreen( worldPoint ) );
@@ -71,8 +62,6 @@ public class CameraZoomWindow extends CameraCommand {
 				return INCOMPLETE;
 			}
 		}
-
-		// The situation of one parameter and an event should not occur
 
 		// Get the world point from the second parameter
 		if( paramCount == 2 ) {
@@ -93,14 +82,18 @@ public class CameraZoomWindow extends CameraCommand {
 		Point3D anchor = context.getScreenAnchor();
 		Point3D mouse = new Point3D( event.getX(), event.getY(), event.getZ() );
 
-		if( event.getEventType().equals( MouseEvent.MOUSE_DRAGGED ) ) {
+		//		if( event.getEventType().equals( MouseEvent.MOUSE_DRAGGED ) ) {
+		//			tool.setSelectAperture( anchor, mouse );
+		//		} else
+
+		if( getStep() == 2 && event.getEventType().equals( MouseEvent.MOUSE_MOVED ) ) {
 			tool.setSelectAperture( anchor, mouse );
-		} else if( getStep() == 3 && event.getEventType().equals( MouseEvent.MOUSE_MOVED ) ) {
-			tool.setSelectAperture( anchor, mouse );
-		} else if( event.getEventType().equals( MouseEvent.MOUSE_RELEASED ) ) {
-			// Submit a Value command to pass the point back to this command
-			tool.getCommandContext().submit( tool, new Value(), tool.screenToWorld( mouse ) );
 		}
+
+		//			else if( event.getEventType().equals( MouseEvent.MOUSE_RELEASED ) ) {
+		//			// Submit a Value command to pass the point back to this command
+		//			tool.getCommandContext().submit( tool, new Value(), tool.screenToWorld( mouse ) );
+		//		}
 	}
 
 }
