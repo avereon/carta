b4b987d9accc974481ddca39036172d93340684e
msoderquist <msoderquist@mastercontrol.com>
2024-08-09 08:33:16 -0600

    Update CameraZoomAll command

--- source/main/java/com/avereon/cartesia/command/camera/CameraZoomAll.java
+++ source/main/java/com/avereon/cartesia/command/camera/CameraZoomAll.java
@@ -1,14 +1,12 @@
 package com.avereon.cartesia.command.camera;
 
-import com.avereon.cartesia.CommandTrigger;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import javafx.scene.input.InputEvent;
+import com.avereon.cartesia.tool.CommandTask;
 
 public class CameraZoomAll extends CameraCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-		return zoomShapes( context, context.getTool().getVisibleShapes() );
+	public Object execute( CommandTask task) throws Exception {
+		return zoomShapes( task.getContext(), task.getTool().getVisibleShapes() );
 	}
 
 }
