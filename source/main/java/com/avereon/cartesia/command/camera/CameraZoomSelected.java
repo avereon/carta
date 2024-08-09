a9d61717f4ef988f3985e8797184c581db9f793c
msoderquist <msoderquist@mastercontrol.com>
2024-08-09 08:35:37 -0600

    Update CameraZoomSelected command

--- source/main/java/com/avereon/cartesia/command/camera/CameraZoomSelected.java
+++ source/main/java/com/avereon/cartesia/command/camera/CameraZoomSelected.java
@@ -1,15 +1,12 @@
 package com.avereon.cartesia.command.camera;
 
-import com.avereon.cartesia.CommandTrigger;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import javafx.scene.input.InputEvent;
-import static com.avereon.cartesia.command.Command.Result.*;
+import com.avereon.cartesia.tool.CommandTask;
 
 public class CameraZoomSelected extends CameraCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-		return zoomShapes( context, context.getTool().getSelectedShapes() );
+	public Object execute( CommandTask task ) throws Exception {
+		return zoomShapes( task.getContext(), task.getTool().getSelectedShapes() );
 	}
 
 }
---
+++ source/test/java/com/avereon/cartesia/command/camera/CameraZoomSelectedTest.java
@@ -0,0 +1,33 @@
+package com.avereon.cartesia.command.camera;
+
+import com.avereon.cartesia.CommandBaseTest;
+import com.avereon.cartesia.command.Command;
+import com.avereon.cartesia.data.DesignLine;
+import com.avereon.cartesia.tool.CommandTask;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class CameraZoomSelectedTest extends CommandBaseTest {
+
+	private final Command command = new CameraZoomSelected();
+
+	@Test
+	void execute() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command );
+		when( tool.getSelectedShapes() ).thenReturn( List.of( new DesignLine( -1, -1, 1, 1 ) ) );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		verify( tool, times( 1 ) ).setScreenViewport( any() );
+		assertThat( result ).isEqualTo( Command.Result.SUCCESS );
+	}
+
+}
