311f01c32e5bc9f166e70dae13d2ce2c34862a9d
msoderquist <msoderquist@mastercontrol.com>
2024-08-16 10:13:34 -0600

    Update DrawCircle3 command to use InvalidInputException

--- source/main/java/com/avereon/cartesia/command/draw/DrawCircle3.java
+++ source/main/java/com/avereon/cartesia/command/draw/DrawCircle3.java
@@ -1,27 +1,19 @@
 package com.avereon.cartesia.command.draw;
 
-import com.avereon.cartesia.CommandTrigger;
-import com.avereon.cartesia.RbKey;
+import com.avereon.cartesia.command.CommandTask;
 import com.avereon.cartesia.data.DesignArc;
 import com.avereon.cartesia.data.DesignEllipse;
 import com.avereon.cartesia.data.DesignLine;
 import com.avereon.cartesia.math.CadGeometry;
 import com.avereon.cartesia.tool.BaseDesignTool;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import com.avereon.product.Rb;
-import com.avereon.xenon.notice.Notice;
 import javafx.geometry.Point3D;
-import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
 
-import java.text.ParseException;
-
-import static com.avereon.cartesia.command.Command.Result.INCOMPLETE;
-import static com.avereon.cartesia.command.Command.Result.SUCCESS;
+import static com.avereon.cartesia.command.Command.Result.*;
 
 public class DrawCircle3 extends DrawCommand {
 
-	private DesignLine previewLine;
+	private DesignLine referenceLine;
 
 	private DesignEllipse previewEllipse;
 
@@ -30,64 +22,63 @@ public class DrawCircle3 extends DrawCommand {
 	private Point3D mid;
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-		setCaptureUndoChanges( context, false );
+	public Object execute( CommandTask task ) throws Exception {
+		setCaptureUndoChanges( task, false );
 
 		// Step 1
-		if( parameters.length < 1 ) {
-			addPreview( context, previewLine = new DesignLine( context.getWorldMouse(), context.getWorldMouse() ) );
-			promptForPoint( context, "start-point" );
+		if( task.getParameterCount() == 0 ) {
+			if( referenceLine == null ) referenceLine = createReferenceLine( task );
+			promptForPoint( task, "start-point" );
 			return INCOMPLETE;
 		}
 
 		// Step 2
-		if( parameters.length < 2 ) {
-			start = asPoint( context, parameters[ 0 ] );
-			previewLine.setOrigin( start );
-			promptForPoint( context, "mid-point" );
+		if( task.getParameterCount() == 1 ) {
+			start = asPoint( task, "start-point", 0 );
+			if( referenceLine == null ) referenceLine = createReferenceLine( task );
+			referenceLine.setOrigin( start );
+			promptForPoint( task, "mid-point" );
 			return INCOMPLETE;
 		}
 
 		// Step 3
-		if( parameters.length < 3 ) {
-			removePreview( context, previewLine );
+		if( task.getParameterCount() == 2 ) {
+			removePreview( task, referenceLine );
 
-			mid = asPoint( context, parameters[ 1 ] );
-			addPreview( context, previewEllipse = CadGeometry.circleFromThreePoints( start, mid, mid ) );
+			start = asPoint( task, "start-point", 0 );
+			mid = asPoint( task, "mid-point", 1 );
+			if( previewEllipse == null ) previewEllipse = createPreviewArc3( task, start, mid );
 
-			promptForPoint( context, "end-point" );
+			promptForPoint( task, "end-point" );
 			return INCOMPLETE;
 		}
 
-		clearReferenceAndPreview( context );
-		setCaptureUndoChanges( context, true );
+		if( task.hasParameter( 2 ) ) {
+			setCaptureUndoChanges( task, true );
 
-		try {
-			start = asPoint( context, parameters[ 0 ] );
-			mid = asPoint( context, parameters[ 1 ] );
-			Point3D end = asPoint( context, parameters[ 2 ] );
-			context.getTool().getCurrentLayer().addShape( CadGeometry.circleFromThreePoints( start, mid, end ) );
-		} catch( ParseException exception ) {
-			String title = Rb.text( RbKey.NOTICE, "command-error" );
-			String message = Rb.text( RbKey.NOTICE, "unable-to-create-shape", exception );
-			if( context.isInteractive() ) context.getProgram().getNoticeManager().addNotice( new Notice( title, message ) );
+			start = asPoint( task, "start-point", 0 );
+			mid = asPoint( task, "mid-point", 1 );
+			Point3D end = asPoint( task, "end-point", 2 );
+			task.getTool().getCurrentLayer().addShape( CadGeometry.circleFromThreePoints( start, mid, end ) );
+
+			return SUCCESS;
 		}
 
-		return SUCCESS;
+		return FAILURE;
 	}
 
 	@Override
-	public void handle( DesignCommandContext context, MouseEvent event ) {
+	public void handle( CommandTask task, MouseEvent event ) {
 		if( event.getEventType() == MouseEvent.MOUSE_MOVED ) {
 			BaseDesignTool tool = (BaseDesignTool)event.getSource();
 			Point3D point = tool.screenToWorkplane( event.getX(), event.getY(), event.getZ() );
 
 			switch( getStep() ) {
 				case 1 -> {
-					previewLine.setOrigin( point );
-					previewLine.setPoint( point );
+					referenceLine.setOrigin( point );
+					referenceLine.setPoint( point );
 				}
-				case 2 -> previewLine.setPoint( point );
+				case 2 -> referenceLine.setPoint( point );
 				case 3 -> {
 					DesignArc next = CadGeometry.arcFromThreePoints( start, mid, point );
 					previewEllipse.setOrigin( next.getOrigin() );
