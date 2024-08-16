72d01265d534cedf204a7cf761be3a89d9180d10
msoderquist <msoderquist@mastercontrol.com>
2024-08-16 11:11:44 -0600

    Update unimplemented commands to use CommandTask

--- source/main/java/com/avereon/cartesia/command/draw/DrawConicArc5.java
+++ source/main/java/com/avereon/cartesia/command/draw/DrawConicArc5.java
@@ -1,8 +1,16 @@
 package com.avereon.cartesia.command.draw;
 
+import com.avereon.cartesia.command.CommandTask;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
+
 public class DrawConicArc5 extends DrawCommand {
 
-	// NOTE It turns out that five points define a conic, not just an ellipse
-	// therefore, implementing this command is left for "later"
+	@Override
+	public Object execute( CommandTask task ) throws Exception {
+		// NOTE It turns out that five points define a conic, not just an ellipse
+		// therefore, implementing this command is left for "later"
+		return SUCCESS;
+	}
 
 }
--- source/main/java/com/avereon/cartesia/command/draw/DrawPath.java
+++ source/main/java/com/avereon/cartesia/command/draw/DrawPath.java
@@ -1,14 +1,13 @@
 package com.avereon.cartesia.command.draw;
 
-import com.avereon.cartesia.CommandTrigger;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import javafx.scene.input.InputEvent;
-import static com.avereon.cartesia.command.Command.Result.*;
+import com.avereon.cartesia.command.CommandTask;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
 
 public class DrawPath extends DrawCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
+	public Object execute( CommandTask task ) throws Exception {
 		// This one will be fun because it can be very powerful
 		// Once started it can take SVG-like parameters to draw a path
 		// Of particular interest is that it can be open or closed
