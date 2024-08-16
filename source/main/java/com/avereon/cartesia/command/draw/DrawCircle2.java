f77f39903dab4eaf4a1760eaf5cd850446bd22f3
msoderquist <msoderquist@mastercontrol.com>
2024-08-15 18:10:31 -0600

    Update DrawCircle2 command to use InvalidInputException

--- source/main/java/com/avereon/cartesia/command/draw/DrawCircle2.java
+++ source/main/java/com/avereon/cartesia/command/draw/DrawCircle2.java
@@ -1,22 +1,14 @@
 package com.avereon.cartesia.command.draw;
 
-import com.avereon.cartesia.CommandTrigger;
-import com.avereon.cartesia.RbKey;
+import com.avereon.cartesia.command.CommandTask;
 import com.avereon.cartesia.data.DesignEllipse;
 import com.avereon.cartesia.data.DesignLine;
 import com.avereon.cartesia.tool.BaseDesignTool;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import com.avereon.product.Rb;
-import com.avereon.xenon.notice.Notice;
 import javafx.geometry.Point3D;
-import javafx.scene.input.InputEvent;
 import javafx.scene.input.MouseEvent;
 import lombok.CustomLog;
 
-import java.text.ParseException;
-
-import static com.avereon.cartesia.command.Command.Result.INCOMPLETE;
-import static com.avereon.cartesia.command.Command.Result.SUCCESS;
+import static com.avereon.cartesia.command.Command.Result.*;
 
 @CustomLog
 public class DrawCircle2 extends DrawCommand {
@@ -26,44 +18,41 @@ public class DrawCircle2 extends DrawCommand {
 	private DesignLine previewLine;
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-		setCaptureUndoChanges( context, false );
+	public Object execute( CommandTask task ) throws Exception {
+		setCaptureUndoChanges( task, false );
 
 		// Step 1
-		if( parameters.length < 1 ) {
-			addPreview( context, previewLine = new DesignLine( context.getWorldMouse(), context.getWorldMouse() ) );
-			promptForPoint( context, "center" );
+		if( task.getParameterCount() == 0 ) {
+			previewLine = createReferenceLine( task );
+			promptForPoint( task, "center" );
 			return INCOMPLETE;
 		}
 
 		// Step 2
-		if( parameters.length < 2 ) {
-			Point3D origin = asPoint( context, parameters[ 0 ] );
-			addPreview( context, previewEllipse = new DesignEllipse( origin, 0.0 ) );
+		if( task.getParameterCount() == 1 ) {
+			Point3D origin = asPoint( task, "center", 0 );
+			if( previewEllipse == null ) previewEllipse = createPreviewEllipse( task, origin, 0.0 );
 			previewLine.setOrigin( origin );
 			previewLine.setPoint( origin );
-			promptForNumber( context, "radius" );
+			promptForNumber( task, "radius" );
 			return INCOMPLETE;
 		}
 
-		clearReferenceAndPreview( context );
-		setCaptureUndoChanges( context, true );
+		setCaptureUndoChanges( task, true );
 
-		try {
-			Point3D origin = asPoint( context, parameters[ 0 ] );
-			double radius = asDouble( previewEllipse.getOrigin(), parameters[ 1 ] );
-			context.getTool().getCurrentLayer().addShape( new DesignEllipse( origin, radius ) );
-		} catch( ParseException exception ) {
-			String title = Rb.text( RbKey.NOTICE, "command-error" );
-			String message = Rb.text( RbKey.NOTICE, "unable-to-create-shape", exception );
-			if( context.isInteractive() ) context.getProgram().getNoticeManager().addNotice( new Notice( title, message ) );
+		if( task.hasParameter( 1 ) ) {
+			Point3D origin = asPoint( task, "center", 0 );
+			double radius = asDouble( task, "radius", origin, 1 );
+			task.getTool().getCurrentLayer().addShape( new DesignEllipse( origin, radius ) );
+
+			return SUCCESS;
 		}
 
-		return SUCCESS;
+		return FAILURE;
 	}
 
 	@Override
-	public void handle( DesignCommandContext context, MouseEvent event ) {
+	public void handle( CommandTask task, MouseEvent event ) {
 		if( event.getEventType() == MouseEvent.MOUSE_MOVED ) {
 			BaseDesignTool tool = (BaseDesignTool)event.getSource();
 			Point3D point = tool.screenToWorkplane( event.getX(), event.getY(), event.getZ() );
---
+++ source/test/java/com/avereon/cartesia/command/draw/DrawCircle2Test.java
@@ -0,0 +1,66 @@
+package com.avereon.cartesia.command.draw;
+
+import com.avereon.cartesia.CommandBaseTest;
+import com.avereon.cartesia.command.CommandTask;
+import com.avereon.cartesia.command.Prompt;
+import com.avereon.cartesia.data.DesignEllipse;
+import com.avereon.cartesia.data.DesignLine;
+import javafx.scene.Cursor;
+import org.junit.jupiter.api.Test;
+
+import static com.avereon.cartesia.command.Command.Result.INCOMPLETE;
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class DrawCircle2Test extends CommandBaseTest {
+
+	private final DrawCircle2 command = new DrawCircle2();
+
+	// Script Tests --------------------------------------------------------------
+
+	@Test
+	void testRunTaskStepWithAllParameters() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, "8,3", "1,0" );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		verify( currentLayer, times( 1 ) ).addShape( any( DesignEllipse.class ) );
+		verify( commandContext, times( 0 ) ).submit( eq( tool ), any( Prompt.class ) );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+		assertThat( result ).isEqualTo( SUCCESS );
+	}
+
+	// Interactive Tests ---------------------------------------------------------
+
+	/**
+	 * Draw arc with no parameters or event, should prompt the
+	 * user to select an origin point. The result should be incomplete.
+	 *
+	 * @throws Exception If an error occurs during the test
+	 */
+	@Test
+	void testRunTaskStepNoParameters() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command );
+		// Use the CLOSED_HAND cursor as a reticle cursor
+		when( tool.getReticleCursor() ).thenReturn( Cursor.CLOSED_HAND );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		verify( commandContext, times( 1 ) ).submit( eq( tool ), any( Prompt.class ) );
+		verify( tool, times( 1 ) ).setCursor( Cursor.CLOSED_HAND );
+		assertThat( command.getReference().stream().findFirst().orElse( null ) ).isInstanceOf( DesignLine.class );
+		assertThat( command.getReference() ).hasSize( 1 );
+		assertThat( result ).isEqualTo( INCOMPLETE );
+	}
+
+}
