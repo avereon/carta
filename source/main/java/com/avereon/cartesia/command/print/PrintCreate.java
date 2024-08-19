fcaba4d305751f6913ad8d95944501fb934dc0b3
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 11:58:12 -0600

    Update PrintCreate command

--- source/main/java/com/avereon/cartesia/command/print/PrintCreate.java
+++ source/main/java/com/avereon/cartesia/command/print/PrintCreate.java
@@ -1,30 +1,34 @@
 package com.avereon.cartesia.command.print;
 
-import com.avereon.cartesia.CommandTrigger;
+import com.avereon.cartesia.command.CommandTask;
 import com.avereon.cartesia.data.DesignPrint;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import javafx.scene.input.InputEvent;
-import static com.avereon.cartesia.command.Command.Result.*;
+
+import static com.avereon.cartesia.command.Command.Result.FAILURE;
+import static com.avereon.cartesia.command.Command.Result.INCOMPLETE;
 
 public class PrintCreate extends PrintCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-		if( parameters.length < 1 ) {
-			promptForText( context, "print-name" );
+	public Object execute( CommandTask task ) throws Exception {
+		if( task.getParameterCount() == 0 ) {
+			promptForText( task, "print-name" );
 			return INCOMPLETE;
 		}
 
-		DesignPrint print = new DesignPrint().setName( String.valueOf( parameters[ 0 ] ) );
-		print.setOrigin( context.getTool().getViewPoint() );
-		print.setZoom( context.getTool().getZoom() );
-		print.setRotate( context.getTool().getViewRotate() );
-		print.setLayers( context.getTool().getVisibleLayers() );
+		if( task.hasParameter( 0 ) ) {
+			DesignPrint print = new DesignPrint().setName( asText( task, "print-name", 0 ) );
+			print.setOrigin( task.getTool().getViewPoint() );
+			print.setZoom( task.getTool().getZoom() );
+			print.setRotate( task.getTool().getViewRotate() );
+			print.setLayers( task.getTool().getVisibleLayers() );
 
-		context.getTool().getDesign().addPrint( print );
-		//context.getTool().setCurrentPrint( print );
+			task.getTool().getDesign().addPrint( print );
+			//task.getTool().setCurrentPrint( print );
 
-		return print;
+			return print;
+		}
+
+		return FAILURE;
 	}
 
 }
---
+++ source/test/java/com/avereon/cartesia/command/print/PrintCreateTest.java
@@ -0,0 +1,85 @@
+package com.avereon.cartesia.command.print;
+
+import com.avereon.cartesia.CommandBaseTest;
+import com.avereon.cartesia.command.CommandTask;
+import com.avereon.cartesia.command.InvalidInputException;
+import com.avereon.cartesia.command.Prompt;
+import com.avereon.cartesia.data.DesignPrint;
+import org.junit.jupiter.api.Test;
+
+import static com.avereon.cartesia.command.Command.Result.INCOMPLETE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class PrintCreateTest extends CommandBaseTest {
+
+	private final PrintCreate command = new PrintCreate();
+
+	@Test
+	void testRunTaskStepNoParameters() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		verify( commandContext, times( 1 ) ).submit( eq( tool ), any( Prompt.class ) );
+		verify( tool, times( 1 ) ).setCursor( any() );
+		assertThat( result ).isEqualTo( INCOMPLETE );
+	}
+
+	@Test
+	void testRunTaskStepWithOneParameter() throws Exception {
+		// given
+		String printName = "Custom Print";
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, printName );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		// Check the created view
+		assertThat( result ).isInstanceOf( DesignPrint.class );
+		assertThat( ((DesignPrint)result).getName() ).isEqualTo( printName );
+		verify( design, times( 1 ) ).addPrint( any() );
+		//verify( tool, times( 0 ) ).setCurrentPrint( any() );
+	}
+
+	// Bad Parameter Tests -------------------------------------------------------
+
+	@Test
+	void testRunTaskStepWithBadParameters() {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, new Object[] {null} );
+
+		// when
+		InvalidInputException exception = catchThrowableOfType( InvalidInputException.class, task::runTaskStep );
+
+		// then
+		assertThat( exception.getInputRbKey() ).isEqualTo( "print-name" );
+		verify( design, times( 0 ) ).addPrint( any() );
+		//verify( tool, times( 0 ) ).setCurrentPrint( any() );
+	}
+
+	@Test
+	void testExecuteWithBadParameterTwoIsIgnored() throws Exception {
+		// given
+		String printName = "Custom Print";
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, printName, null );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		assertThat( result ).isInstanceOf( DesignPrint.class );
+		assertThat( ((DesignPrint)result).getName() ).isEqualTo( printName );
+		verify( design, times( 1 ) ).addPrint( any() );
+		//verify( tool, times( 0 ) ).setCurrentPrint( any() );
+	}
+
+}
