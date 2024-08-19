527f1bc4a852023cf8826f544e7f6762bbd4cf60
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 15:48:41 -0600

    Programmer notes

---
+++ source/test/java/com/avereon/cartesia/command/draw/layer/LayerCurrentTest.java
@@ -0,0 +1,12 @@
+package com.avereon.cartesia.command.draw.layer;
+
+import com.avereon.cartesia.CommandBaseTest;
+import com.avereon.cartesia.command.layer.LayerCurrent;
+
+public class LayerCurrentTest extends CommandBaseTest {
+
+	private final LayerCurrent command = new LayerCurrent();
+
+	// NEXT Update LayerCurrent command
+
+}
