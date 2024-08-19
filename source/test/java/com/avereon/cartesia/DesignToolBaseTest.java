6bcc423eb8e6f7eb168f403e9696a6f175420641
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 10:42:23 -0600

    Add snapToWorkplane methods

--- source/test/java/com/avereon/cartesia/DesignToolBaseTest.java
+++ source/test/java/com/avereon/cartesia/DesignToolBaseTest.java
@@ -2,6 +2,7 @@ package com.avereon.cartesia;
 
 import com.avereon.cartesia.tool.DesignTool;
 import com.avereon.xenon.asset.Asset;
+import com.avereon.zarra.javafx.Fx;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mock;
 
@@ -19,6 +20,10 @@ public abstract class DesignToolBaseTest extends BaseCartesiaUnitTest {
 	@BeforeEach
 	protected void setup() throws Exception {
 		super.setup();
+
+		// All tools will want FX started
+		Fx.startup();
+
 		lenient().when( tool.getAsset() ).thenReturn( asset );
 		lenient().when( tool.snapToWorkplane( any() ) ).then( i -> i.getArgument( 0 ) );
 	}
--- source/test/java/com/avereon/cartesia/tool/design/DesignToolV2Test.java
+++ source/test/java/com/avereon/cartesia/tool/design/DesignToolV2Test.java
@@ -1,24 +1,75 @@
 package com.avereon.cartesia.tool.design;
 
 import com.avereon.cartesia.DesignToolBaseTest;
+import com.avereon.cartesia.test.Point3DAssert;
 import javafx.geometry.Point3D;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.avereon.cartesia.test.Point3DAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DesignToolV2Test extends DesignToolBaseTest {
+
+	private static final double DPC = 72 / 2.54;
 
 	private DesignToolV2 tool;
 
 	@BeforeEach
-	void beforeEach() {
+	protected void setup() throws Exception {
+		super.setup();
 		tool = new DesignToolV2( module, asset );
 	}
 
 	@Test
-	void testScreenToWorkplane() {
-		assertThat( tool.screenToWorkplane( 72, -72, 0 ) ).isEqualTo( new Point3D( 2.5, 2.5, 0 ) );
+	void testScreenToWorkplaneWithCoordinates() {
+		// given
+		assertThat( tool.isGridSnapEnabled() ).isTrue();
+		assertThat( tool.getWorkplane().calcSnapGridX() ).isEqualTo( 0.1 );
+		assertThat( tool.getWorkplane().calcSnapGridY() ).isEqualTo( 0.1 );
+
+		// then
+		Point3DAssert.assertThat( tool.screenToWorkplane( 2 * DPC, -2 * DPC, 0 ) ).isEqualTo( new Point3D( 2.0, 2.0, 0 ) );
+		Point3DAssert.assertThat( tool.screenToWorkplane( 2.001 * DPC, -2.002 * DPC, 0 ) ).isEqualTo( new Point3D( 2.0, 2.0, 0 ) );
+		Point3DAssert.assertThat( tool.screenToWorkplane( 2.101 * DPC + 0.1, -2.101 * DPC, 0 ) ).isEqualTo( new Point3D( 2.1, 2.1, 0 ) );
+	}
+
+	@Test
+	void testScreenToWorkplaneWithPoints() {
+		// given
+		assertThat( tool.isGridSnapEnabled() ).isTrue();
+		assertThat( tool.getWorkplane().calcSnapGridX() ).isEqualTo( 0.1 );
+		assertThat( tool.getWorkplane().calcSnapGridY() ).isEqualTo( 0.1 );
+
+		// then
+		Point3DAssert.assertThat( tool.screenToWorkplane( new Point3D( 2 * DPC, -2 * DPC, 0 ) ) ).isEqualTo( new Point3D( 2.0, 2.0, 0 ) );
+		Point3DAssert.assertThat( tool.screenToWorkplane( new Point3D( 2.001 * DPC, -2.001 * DPC, 0 ) ) ).isEqualTo( new Point3D( 2.0, 2.0, 0 ) );
+		Point3DAssert.assertThat( tool.screenToWorkplane( new Point3D( 2.101 * DPC + 0.1, -2.101 * DPC, 0 ) ) ).isEqualTo( new Point3D( 2.1, 2.1, 0 ) );
+	}
+
+	@Test
+	void testSnapToWorkplaneWithCoordinates() {
+		// given
+		assertThat( tool.isGridSnapEnabled() ).isTrue();
+		assertThat( tool.getWorkplane().calcSnapGridX() ).isEqualTo( 0.1 );
+		assertThat( tool.getWorkplane().calcSnapGridY() ).isEqualTo( 0.1 );
+
+		// then
+		Point3DAssert.assertThat( tool.snapToWorkplane( 2, 2, 0 ) ).isEqualTo( new Point3D( 2.0, 2.0, 0 ) );
+		Point3DAssert.assertThat( tool.snapToWorkplane( 2.01, 2.01, 0 ) ).isEqualTo( new Point3D( 2.0, 2.0, 0 ) );
+		Point3DAssert.assertThat( tool.snapToWorkplane( 2.101, 2.101, 0 ) ).isEqualTo( new Point3D( 2.1, 2.1, 0 ) );
+	}
+
+	@Test
+	void testSnapToWorkplaneWithPoints() {
+		// given
+		assertThat( tool.isGridSnapEnabled() ).isTrue();
+		assertThat( tool.getWorkplane().calcSnapGridX() ).isEqualTo( 0.1 );
+		assertThat( tool.getWorkplane().calcSnapGridY() ).isEqualTo( 0.1 );
+
+		// then
+		Point3DAssert.assertThat( tool.snapToWorkplane( new Point3D( 2, 2, 0 ) ) ).isEqualTo( new Point3D( 2.0, 2.0, 0 ) );
+		Point3DAssert.assertThat( tool.snapToWorkplane( new Point3D( 2.01, 2.01, 0 ) ) ).isEqualTo( new Point3D( 2.0, 2.0, 0 ) );
+		Point3DAssert.assertThat( tool.snapToWorkplane( new Point3D( 2.101, 2.101, 0 ) ) ).isEqualTo( new Point3D( 2.1, 2.1, 0 ) );
 	}
 
 }
