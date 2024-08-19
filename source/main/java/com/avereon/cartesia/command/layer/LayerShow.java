b5272d9d482f602d8d96659e124f0d2b2321de11
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 17:55:06 -0600

    Update LayerShow command

--- source/main/java/com/avereon/cartesia/command/layer/LayerShow.java
+++ source/main/java/com/avereon/cartesia/command/layer/LayerShow.java
@@ -1,15 +1,14 @@
 package com.avereon.cartesia.command.layer;
 
-import com.avereon.cartesia.CommandTrigger;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import javafx.scene.input.InputEvent;
-import static com.avereon.cartesia.command.Command.Result.*;
+import com.avereon.cartesia.command.CommandTask;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
 
 public class LayerShow extends LayerCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-		context.getTool().setLayerVisible( context.getTool().getSelectedLayer(), true );
+	public Object execute( CommandTask task ) throws Exception {
+		task.getTool().setLayerVisible( task.getTool().getSelectedLayer(), true );
 		return SUCCESS;
 	}
 
---
+++ source/test/java/com/avereon/cartesia/command/layer/LayerShowTest.java
@@ -0,0 +1,57 @@
+package com.avereon.cartesia.command.layer;
+
+import com.avereon.cartesia.CommandBaseTest;
+import com.avereon.cartesia.command.CommandTask;
+import org.junit.jupiter.api.Test;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class LayerShowTest extends CommandBaseTest {
+
+	private final LayerShow command = new LayerShow();
+
+	// Script Tests --------------------------------------------------------------
+
+	/**
+	 * Layer create with all parameters should create and add a new layer to the
+	 * design. The result should be success.
+	 *
+	 * @throws Exception If an error occurs during the test
+	 */
+	@Test
+	void testExecuteWithAllParameters() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		assertThat( result ).isEqualTo( SUCCESS );
+		verify( tool, times( 1 ) ).setLayerVisible( eq( selectedLayer ), eq( true ) );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+	}
+
+	// Bad Parameter Tests -------------------------------------------------------
+
+	@Test
+	void testExecuteWithBadParameterOneIsIgnored() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, BAD_PARAMETER );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		assertThat( result ).isEqualTo( SUCCESS );
+		verify( tool, times( 1 ) ).setLayerVisible( eq( selectedLayer ), eq( true ) );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+	}
+
+}
