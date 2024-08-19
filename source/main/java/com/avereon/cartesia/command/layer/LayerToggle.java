a2b9080f1096ef0a1cfd156295c4cd72733240f0
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 17:55:14 -0600

    Update LayerToggle command

--- source/main/java/com/avereon/cartesia/command/layer/LayerToggle.java
+++ source/main/java/com/avereon/cartesia/command/layer/LayerToggle.java
@@ -1,17 +1,16 @@
 package com.avereon.cartesia.command.layer;
 
-import com.avereon.cartesia.CommandTrigger;
+import com.avereon.cartesia.command.CommandTask;
 import com.avereon.cartesia.data.DesignLayer;
-import com.avereon.cartesia.tool.DesignCommandContext;
 import com.avereon.cartesia.tool.DesignTool;
-import javafx.scene.input.InputEvent;
-import static com.avereon.cartesia.command.Command.Result.*;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
 
 public class LayerToggle extends LayerCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-		DesignTool tool = context.getTool();
+	public Object execute( CommandTask task) throws Exception {
+		DesignTool tool = task.getTool();
 		DesignLayer layer = tool.getSelectedLayer();
 		tool.setLayerVisible( layer, !tool.isLayerVisible( layer ) );
 		return SUCCESS;
---
+++ source/test/java/com/avereon/cartesia/command/layer/LayerToggleTest.java
@@ -0,0 +1,81 @@
+package com.avereon.cartesia.command.layer;
+
+import com.avereon.cartesia.CommandBaseTest;
+import com.avereon.cartesia.command.CommandTask;
+import org.junit.jupiter.api.Test;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class LayerToggleTest  extends CommandBaseTest {
+
+	private final LayerToggle command = new LayerToggle();
+
+	// Script Tests --------------------------------------------------------------
+
+	/**
+	 * Layer toggle with all parameters should toggle the visibility of the
+	 * selected layer. The result should be success.
+	 *
+	 * @throws Exception If an error occurs during the test
+	 */
+	@Test
+	void testExecuteWithAllParametersWithVisibleTrue() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command );
+		when( tool.isLayerVisible( selectedLayer ) ).thenReturn( true );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		assertThat( result ).isEqualTo( SUCCESS );
+		verify( tool, times( 1 ) ).setLayerVisible( eq( selectedLayer ), eq( false ) );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+	}
+
+	/**
+	 * Layer toggle with all parameters should toggle the visibility of the
+	 * selected layer. The result should be success.
+	 *
+	 * @throws Exception If an error occurs during the test
+	 */
+	@Test
+	void testExecuteWithAllParametersWithVisibleFalse() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command );
+		when( tool.isLayerVisible( selectedLayer ) ).thenReturn( false );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		assertThat( result ).isEqualTo( SUCCESS );
+		verify( tool, times( 1 ) ).setLayerVisible( eq( selectedLayer ), eq( true ) );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+	}
+
+	// Bad Parameter Tests -------------------------------------------------------
+
+	@Test
+	void testExecuteWithBadParameterOneIsIgnored() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, BAD_PARAMETER );
+		when( tool.isLayerVisible( selectedLayer ) ).thenReturn( false );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		assertThat( result ).isEqualTo( SUCCESS );
+		verify( tool, times( 1 ) ).setLayerVisible( eq( selectedLayer ), eq( true ) );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+	}
+
+}
+
