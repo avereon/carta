5682e8cbde26f5430e2bdccbfb2fb6eb4e2a5fa1
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 12:12:42 -0600

    Update PrintUpdate command

--- source/main/java/com/avereon/cartesia/command/print/PrintUpdate.java
+++ source/main/java/com/avereon/cartesia/command/print/PrintUpdate.java
@@ -1,21 +1,20 @@
 package com.avereon.cartesia.command.print;
 
-import com.avereon.cartesia.CommandTrigger;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import javafx.scene.input.InputEvent;
-import static com.avereon.cartesia.command.Command.Result.*;
+import com.avereon.cartesia.command.CommandTask;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
 
 public class PrintUpdate extends PrintCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-//		DesignPrint print = context.getTool().getSelectedPrint();
+	public Object execute( CommandTask task ) throws Exception {
+//		DesignPrint print = task.getTool().getSelectedPrint();
 //		if( print == null ) return COMPLETE;
 //
-//		print.setOrigin( context.getTool().getViewPoint() );
-//		print.setZoom( context.getTool().getZoom() );
-//		print.setRotate( context.getTool().getViewRotate() );
-//		print.setLayers( context.getTool().getVisibleLayers() );
+//		print.setOrigin( task.getTool().getViewPoint() );
+//		print.setZoom( task.getTool().getZoom() );
+//		print.setRotate( task.getTool().getViewRotate() );
+//		print.setLayers( task.getTool().getVisibleLayers() );
 
 		return SUCCESS;
 	}
---
+++ source/test/java/com/avereon/cartesia/command/print/PrintUpdateTest.java
@@ -0,0 +1,46 @@
+package com.avereon.cartesia.command.print;
+
+import com.avereon.cartesia.CommandBaseTest;
+import com.avereon.cartesia.command.CommandTask;
+import org.junit.jupiter.api.Test;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PrintUpdateTest  extends CommandBaseTest {
+
+	private final PrintUpdate command = new PrintUpdate();
+
+	@Test
+	void testRunTaskStepNoParameters() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		//verify( commandContext, times( 1 ) ).submit( eq( tool ), any( Prompt.class ) );
+		//verify( tool, times( 1 ) ).setCursor( any() );
+		assertThat( result ).isEqualTo( SUCCESS );
+	}
+
+	@Test
+	void testRunTaskStepWithOneParameter() throws Exception {
+		// given
+		String printName = "Custom Print";
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, printName );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		// Check the created view
+		//assertThat( result ).isInstanceOf( DesignPrint.class );
+		//assertThat( ((DesignPrint)result).getName() ).isEqualTo( printName );
+		//verify( design, times( 1 ) ).addPrint( any() );
+		//verify( tool, times( 0 ) ).setCurrentPrint( any() );
+		assertThat( result ).isEqualTo( SUCCESS );
+	}
+
+}
