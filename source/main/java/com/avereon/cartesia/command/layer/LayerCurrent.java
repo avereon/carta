f8683ba3d58223e05b9978df543f2c01e2c30efb
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 17:06:07 -0600

    Update LayerCurrent command

--- source/main/java/com/avereon/cartesia/command/layer/LayerCurrent.java
+++ source/main/java/com/avereon/cartesia/command/layer/LayerCurrent.java
@@ -1,20 +1,17 @@
 package com.avereon.cartesia.command.layer;
 
-import com.avereon.cartesia.CommandTrigger;
+import com.avereon.cartesia.command.CommandTask;
 import com.avereon.cartesia.data.DesignLayer;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import javafx.scene.input.InputEvent;
-import static com.avereon.cartesia.command.Command.Result.*;
 
 public class LayerCurrent extends LayerCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
+	public Object execute( CommandTask task ) throws Exception {
 		// Get the selected layer
-		DesignLayer yy = context.getTool().getSelectedLayer();
+		DesignLayer yy = task.getTool().getSelectedLayer();
 
 		// Make the layer selected in the guide the current layer
-		context.getTool().setCurrentLayer( yy );
+		task.getTool().setCurrentLayer( yy );
 
 		return yy;
 	}
--- source/test/java/com/avereon/cartesia/command/draw/layer/LayerCurrentTest.java
+++ source/test/java/com/avereon/cartesia/command/draw/layer/LayerCurrentTest.java
@@ -1,12 +1,59 @@
 package com.avereon.cartesia.command.draw.layer;
 
 import com.avereon.cartesia.CommandBaseTest;
+import com.avereon.cartesia.command.CommandTask;
 import com.avereon.cartesia.command.layer.LayerCurrent;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class LayerCurrentTest extends CommandBaseTest {
 
+	public static final Object BAD_PARAMETER = null;
+
 	private final LayerCurrent command = new LayerCurrent();
 
-	// NEXT Update LayerCurrent command
+	// Script Tests --------------------------------------------------------------
+
+	/**
+	 * Layer current with all parameters should make the selected layer the
+	 * current. The result should be success.
+	 *
+	 * @throws Exception If an error occurs during the test
+	 */
+	@Test
+	void testExecuteWithAllParameters() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		verify( tool, times( 1 ) ).setCurrentLayer( eq( selectedLayer ) );
+		assertThat( result ).isEqualTo( selectedLayer );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+	}
+
+	// Bad Parameter Tests -------------------------------------------------------
+
+	@Test
+	void testExecuteWithBadParameterOneIsIgnored() throws Exception {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, BAD_PARAMETER );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		verify( tool, times( 1 ) ).setCurrentLayer( eq( selectedLayer ) );
+		assertThat( result ).isEqualTo( selectedLayer );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+	}
 
 }
