8d8e1a23dbec96cb233932225d2fe1aa41c4d08a
msoderquist <msoderquist@mastercontrol.com>
2024-08-20 15:08:04 -0600

    Update Move/Copy command

--- source/test/java/com/avereon/cartesia/command/edit/CopyTest.java
+++ source/test/java/com/avereon/cartesia/command/edit/CopyTest.java
@@ -2,16 +2,25 @@ package com.avereon.cartesia.command.edit;
 
 import com.avereon.cartesia.CommandBaseTest;
 import com.avereon.cartesia.command.CommandTask;
+import com.avereon.cartesia.command.InvalidInputException;
+import com.avereon.cartesia.command.Prompt;
 import com.avereon.cartesia.data.DesignLayer;
 import com.avereon.cartesia.data.DesignLine;
 import javafx.geometry.Point3D;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 import static com.avereon.cartesia.command.Command.Result.SUCCESS;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.catchThrowableOfType;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 public class CopyTest extends CommandBaseTest {
 
@@ -38,20 +47,7 @@ public class CopyTest extends CommandBaseTest {
 		// when
 		Object result = task.runTaskStep();
 
-		// then
-		assertThat( result ).isEqualTo( SUCCESS );
-		assertThat( command.getReference() ).hasSize( 0 );
-		assertThat( command.getPreview() ).hasSize( 0 );
-
-		// The original line should not move
-		assertThat( line.getOrigin() ).isEqualTo( new Point3D( 0, 0, 0 ) );
-		assertThat( line.getPoint() ).isEqualTo( new Point3D( 0, 10, 0 ) );
-
-		// But there should be a second line that is in the new location
-		DesignLine newLine = (DesignLine)layer.getShapes().stream().filter( s -> s != line ).findFirst().orElse( null );
-		assertThat( newLine ).isNotNull();
-		assertThat( newLine.getOrigin() ).isEqualTo( new Point3D( 1, 0, 0 ) );
-		assertThat( newLine.getPoint() ).isEqualTo( new Point3D( 1, 10, 0 ) );
+		assertSuccessfulCopy( result, layer, line );
 	}
 
 	// Interactive Tests ---------------------------------------------------------
@@ -71,6 +67,65 @@ public class CopyTest extends CommandBaseTest {
 	}
 
 	// Bad Parameter Tests -------------------------------------------------------
+
+	@ParameterizedTest
+	@MethodSource( "provideParametersForTestWithParameters" )
+	void testRunTaskStepWithBadParameters( Object[] parameters, String rbKey ) {
+		// given
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, parameters );
+		when( tool.getSelectedShapes() ).thenReturn( List.of( new DesignLine() ) );
+
+		// when
+		InvalidInputException exception = catchThrowableOfType( InvalidInputException.class, task::runTaskStep );
+
+		// then
+		verify( commandContext, times( 0 ) ).submit( eq( tool ), any( Prompt.class ) );
+		verify( currentLayer, times( 0 ) ).addShape( any() );
+		assertThat( exception.getInputRbKey() ).isEqualTo( rbKey );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+	}
+
+	private static Stream<Arguments> provideParametersForTestWithParameters() {
+		return Stream.of(
+			Arguments.of( new Object[]{ BAD_POINT_PARAMETER }, "anchor" ),
+			Arguments.of( new Object[]{ "-3,3", BAD_POINT_PARAMETER }, "target" )
+		);
+	}
+
+	@Test
+	void testExecuteWithBadParameterThreeIsIgnored() throws Exception {
+		DesignLine line = new DesignLine( 0, 0, 0, 10 );
+		DesignLayer layer = new DesignLayer();
+		layer.addShape( line );
+		CommandTask task = new CommandTask( commandContext, tool, null, null, command, "1,2", "2,2" );
+
+		// Selected shapes are required for this command
+		when( tool.getSelectedShapes() ).thenReturn( List.of( line ) );
+
+		// when
+		Object result = task.runTaskStep();
+
+		// then
+		assertSuccessfulCopy( result, layer, line );
+	}
+
+	private void assertSuccessfulCopy( Object result, DesignLayer layer, DesignLine line ) {
+		// then
+		assertThat( result ).isEqualTo( SUCCESS );
+		assertThat( command.getReference() ).hasSize( 0 );
+		assertThat( command.getPreview() ).hasSize( 0 );
+
+		// The original line should not move
+		assertThat( line.getOrigin() ).isEqualTo( new Point3D( 0, 0, 0 ) );
+		assertThat( line.getPoint() ).isEqualTo( new Point3D( 0, 10, 0 ) );
+
+		// But there should be a second line that is in the new location
+		DesignLine newLine = (DesignLine)layer.getShapes().stream().filter( s -> s != line ).findFirst().orElse( null );
+		assertThat( newLine ).isNotNull();
+		assertThat( newLine.getOrigin() ).isEqualTo( new Point3D( 1, 0, 0 ) );
+		assertThat( newLine.getPoint() ).isEqualTo( new Point3D( 1, 10, 0 ) );
+	}
 
 }
 
