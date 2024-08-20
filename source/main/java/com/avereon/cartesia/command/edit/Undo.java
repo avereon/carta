4445f3ef7f815c022ebfde35d88e1045d08ca0c5
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 18:13:51 -0600

    Update Undo command

--- source/main/java/com/avereon/cartesia/command/edit/Undo.java
+++ source/main/java/com/avereon/cartesia/command/edit/Undo.java
@@ -1,21 +1,14 @@
 package com.avereon.cartesia.command.edit;
 
-import com.avereon.cartesia.CommandTrigger;
-import com.avereon.cartesia.tool.DesignCommandContext;
-import javafx.scene.input.InputEvent;
-import static com.avereon.cartesia.command.Command.Result.*;
+import com.avereon.cartesia.command.CommandTask;
+
+import static com.avereon.cartesia.command.Command.Result.SUCCESS;
 
 public class Undo extends EditCommand {
 
 	@Override
-	public Object execute( DesignCommandContext context, CommandTrigger trigger, InputEvent triggerEvent, Object... parameters ) throws Exception {
-//		if( context.getTool().selectedShapes().isEmpty() ) return COMPLETE;
-//
-//		clearReferenceAndPreview( context );
-//		setCaptureUndoChanges( context, true );
-//
-//		deleteShapes( getCommandShapes( context.getTool() ) );
-
+	public Object execute( CommandTask task ) throws Exception {
+		task.getContext().getTool().getAsset().getUndoManager().undo();
 		return SUCCESS;
 	}
 
