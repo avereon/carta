42d8b53a2e6f17ea2b5850a1ec7640b18451620b
msoderquist <msoderquist@mastercontrol.com>
2024-08-19 18:52:00 -0600

    Improve documentation

--- source/main/java/com/avereon/cartesia/math/Trim.java
+++ source/main/java/com/avereon/cartesia/math/Trim.java
@@ -16,6 +16,15 @@ import java.util.List;
 @CustomLog
 public class Trim extends CadEdit{
 
+	/**
+	 * Trim the trim shape to the edge shape.
+	 *
+	 * @param tool The design tool
+	 * @param trim The trim shape
+	 * @param edge The edge shape
+	 * @param trimPoint The point used to select the trim shape, in screen coordinates
+	 * @param edgePoint The point used to select the edge shape, in screen coordinates
+	 */
 	public static void trim( DesignTool tool, DesignShape trim, DesignShape edge, Point3D trimPoint, Point3D edgePoint ) {
 		List<Point3D> intersections = CadIntersection.getIntersections( trim, edge );
 		Point3D target = CadPoints.getNearestOnScreen( tool, edgePoint, intersections );
