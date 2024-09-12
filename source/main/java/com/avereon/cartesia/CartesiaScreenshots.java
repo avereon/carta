ffdcbe957d7db62eecfd42dcb7983709b72367e8
msoderquist <msoderquist@mastercontrol.com>
2024-09-12 11:03:45 -0600

    Work on mod screenshots

---
+++ source/main/java/com/avereon/cartesia/CartesiaScreenshots.java
@@ -0,0 +1,18 @@
+package com.avereon.cartesia;
+
+import com.avereon.xenon.ProgramScreenshots;
+
+import java.util.concurrent.TimeoutException;
+
+public class CartesiaScreenshots extends ProgramScreenshots {
+
+	public static void main( String[] args ) {
+		new CartesiaScreenshots().generate( args );
+	}
+
+	@Override
+	protected void generateScreenshots() throws InterruptedException, TimeoutException {
+		// TODO Implement Cartesia screenshots
+	}
+
+}
