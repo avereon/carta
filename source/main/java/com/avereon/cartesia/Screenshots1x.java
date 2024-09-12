19a867e358bfe209618dd84952e730cb7805bb43
msoderquist <msoderquist@mastercontrol.com>
2024-09-11 20:30:14 -0600

    Work on mod screenshots

---
+++ source/main/java/com/avereon/cartesia/Screenshots1x.java
@@ -0,0 +1,17 @@
+package com.avereon.cartesia;
+
+public class Screenshots1x {
+
+	/*
+	 Xenon has a class named Screenshots that is used to generate screenshots
+	 of the application. The Screenshots class has a method named generate that
+	 takes an integer argument.
+
+	 Refactor the Screenshots class to be useful for Cartesia and other mods.
+	 */
+
+	public static void main( String[] args ) {
+		//
+	}
+
+}
---
+++ source/main/java/com/avereon/cartesia/Screenshots2x.java
@@ -0,0 +1,9 @@
+package com.avereon.cartesia;
+
+public class Screenshots2x {
+
+	public static void main( String[] args ) {
+		//
+	}
+
+}
