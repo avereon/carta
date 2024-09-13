4a4a5338090801782947ffac6069d3906df7d666
msoderquist <msoderquist@mastercontrol.com>
2024-09-12 20:46:50 -0600

    Improve screenshot API

--- source/main/java/com/avereon/cartesia/CartesiaScreenshots.java
+++ source/main/java/com/avereon/cartesia/CartesiaScreenshots.java
@@ -2,7 +2,13 @@ package com.avereon.cartesia;
 
 import com.avereon.product.ProgramFlag;
 import com.avereon.xenon.ProgramScreenshots;
+import com.avereon.xenon.workpane.Tool;
+import com.avereon.zarra.javafx.Fx;
+import javafx.geometry.Bounds;
+import javafx.scene.input.MouseButton;
+import javafx.scene.robot.Robot;
 
+import java.net.URI;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +20,10 @@ public class CartesiaScreenshots extends ProgramScreenshots {
 		new CartesiaScreenshots().generate( args );
 	}
 
-//	@Override
-//	protected String getLogLevel() {
-//		return ProgramFlag.DEBUG;
-//	}
+	//	@Override
+	//	protected String getLogLevel() {
+	//		return ProgramFlag.DEBUG;
+	//	}
 
 	@Override
 	protected List<String> getProgramParameters() {
@@ -31,7 +37,19 @@ public class CartesiaScreenshots extends ProgramScreenshots {
 
 	@Override
 	protected void generateScreenshots() throws InterruptedException, TimeoutException {
-		screenshot( Path.of( "sample/design/screenshot.cartesia2d" ).toUri(), "design-tool" );
+		generateDesignToolSnapshot( Path.of( "sample/design/screenshot.cartesia2d" ).toUri(), "design-tool" );
+	}
+
+	private void generateDesignToolSnapshot( URI uri, String name ) throws InterruptedException, TimeoutException {
+		openAsset( uri );
+		Fx.run( () -> {
+			Robot robot = new Robot();
+			Tool tool = getProgram().getWorkspaceManager().getActiveWorkspace().getActiveWorkarea().getWorkpane().getActiveTool();
+			Bounds b = tool.localToScreen( tool.getLayoutBounds() );
+			robot.mouseMove( b.getCenterX(), b.getCenterY() );
+			robot.mouseClick( MouseButton.PRIMARY );
+		} );
+		screenshot( name );
 	}
 
 }
