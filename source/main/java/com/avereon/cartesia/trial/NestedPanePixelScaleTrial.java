4140ba9971236ffe1f6751f110344dadddc7c17b
Mark Soderquist <mark@soderquist.net>
2025-06-09 08:19:20 -0600

    Refactor `NestedPanePixelScaleTrial`: enhance scaling functionality, add `Pane` hierarchy, and update visuals with improved layout, scaling adjustments, and debugging support.

--- source/main/java/com/avereon/cartesia/trial/NestedPanePixelScaleTrial.java
+++ source/main/java/com/avereon/cartesia/trial/NestedPanePixelScaleTrial.java
@@ -1,9 +1,13 @@
 package com.avereon.cartesia.trial;
 
+import com.avereon.util.ThreadUtil;
+import com.avereon.zerra.javafx.Fx;
+import com.avereon.zerra.javafx.FxUtil;
 import javafx.application.Application;
+import javafx.geometry.Bounds;
 import javafx.scene.Scene;
-import javafx.scene.layout.Border;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeLineCap;
@@ -11,10 +15,34 @@ import javafx.stage.Stage;
 
 public class NestedPanePixelScaleTrial extends Application {
 
+	private Stage stage;
+
+	private Scene scene;
+
+	private Pane tool;
+
+	private Pane glass;
+
+	private Pane world;
+
+	private Pane scaledWorld;
+
 	@Override
 	public void start( Stage stage ) throws Exception {
+		this.stage = stage;
+
+		double scale = 80;
+		double width = 500;
+		double height = 500;
+
+		// This should be as simple as:
+		// 1. Move the origin from upper-left to center using pixels
+		// 2. Scale according to the desired design scale
+		// 3. Flip the Y-axis. In theory, this should be done in step 2 also.
+
+		// The unscaled world
 		Line line1 = new Line( -2, -2, 2, 2 );
-		line1.setStroke( javafx.scene.paint.Color.RED );
+		line1.setStroke( javafx.scene.paint.Color.RED.darker().darker() );
 		line1.setStrokeWidth( 1 );
 		line1.setStrokeLineCap( StrokeLineCap.ROUND );
 		Line line2 = new Line( -2, 2, 2, -2 );
@@ -22,38 +50,92 @@ public class NestedPanePixelScaleTrial extends Application {
 		line2.setStrokeWidth( 1 );
 		line2.setStrokeLineCap( StrokeLineCap.ROUND );
 
-		Pane world = new Pane();
-		world.setBorder( Border.EMPTY );
-		world.relocate( 0, 0 );
+		this.world = new Pane();
 		world.getChildren().addAll( line1, line2 );
-		world.resizeRelocate( 0, 0, 2000, 2000 );
+		//world.resizeRelocate( 0, 0, 0,0 );
 
-		double scale = 1;
-		Pane glass = new Pane();
-		glass.setBorder( Border.EMPTY );
-		glass.relocate( 0, 0 );
-		//		glass.setTranslateX( 250 );
-		//		glass.setTranslateY( 250 );
+		// The scaled world
+		Line scaledLine1 = new Line( -2 * scale, -2 * scale, 2 * scale, 2 * scale );
+		scaledLine1.setStroke( javafx.scene.paint.Color.RED.darker().darker() );
+		scaledLine1.setStrokeWidth( 1 * scale );
+		scaledLine1.setStrokeLineCap( StrokeLineCap.ROUND );
+		Line scaledLine2 = new Line( -2 * scale, 2 * scale, 2 * scale, -2 * scale );
+		scaledLine2.setStroke( javafx.scene.paint.Color.GREEN );
+		scaledLine2.setStrokeWidth( 1 * scale );
+		scaledLine2.setStrokeLineCap( StrokeLineCap.ROUND );
+
+		this.scaledWorld = new Pane();
+		scaledWorld.getChildren().addAll( scaledLine1, scaledLine2 );
+		//scaledWorld.resizeRelocate( 0, 0, 0,0 );
+//		scaledWorld.setTranslateX( 1 * scale );
+//		scaledWorld.setTranslateY( 1 * scale );
+
+		// The magnifying glass pane (translation, zoom, rotate)
+		this.glass = new Pane();
+		glass.relocate( 0.5 * width+ 200, 0.5 * height + 200);
+//		glass.setTranslateX( 1 );
+//		glass.setTranslateY( 1 );
 		glass.setScaleX( scale );
-		glass.setScaleY( -scale );
+		glass.setScaleY( scale );
 		glass.getChildren().add( world );
 
-		Pane tool = new Pane();
+		this.tool = new Pane();
 		tool.setStyle( "-fx-background-color: #222222;" );
-		Rectangle border = new Rectangle( 0, 0, 500, 500 );
-		border.setStroke( javafx.scene.paint.Color.GRAY );
+		Rectangle border = new Rectangle( 0.5, 0.5, width, height );
+		border.setFill( Color.TRANSPARENT );
+		border.setStroke( Color.GRAY );
 		tool.getChildren().addAll( border, glass );
 
-		Scene scene = new Scene( tool, 500, 500 );
+		this.scene = new Scene( tool, width, height );
 		stage.setScene( scene );
 
 		stage.setTitle( "Nested Pane Pixel Scale Trial" );
-		stage.show();
-		stage.sizeToScene();
 		stage.centerOnScreen();
+		stage.show();
+		// Apparently trying to set the stage size is inconsistent on Mint with my settings
+		// Stop fighting with it and move on
+		// Stage.sizeToScene can be inconsistent, so set the size directly
+		//stage.sizeToScene();
+		//stage.setWidth( 500 );
+		// The extra 32 is to account for the title bar
+		//stage.setHeight( 500 );
 
-		// NEXT Figure out why the world pane is not at 0,0
-		System.out.println( "World pos: " + world.getLayoutX() + ", " + world.getLayoutY() );
+		//		//System.out.println( "World pos: " + world.getLayoutX() + ", " + world.getLayoutY() );
+		//		System.out.println( "Stage size: " + stage.getWidth() + ", " + stage.getHeight() );
+		//		System.out.println( "Scene size: " + scene.getWidth() + ", " + scene.getHeight() );
+		//		System.out.println( "Tool size:  " + tool.getWidth() + ", " + tool.getHeight() );
+		//
+
+		//		tool.widthProperty().addListener( ( _, _, _ ) -> printBounds() );
+		//		tool.heightProperty().addListener( ( _, _, _ ) -> printBounds() );
+
+		new Thread( () -> {
+			ThreadUtil.pause( 100 );
+			printBounds();
+
+			Fx.run( () -> {
+				//				Bounds worldToTool = FxUtil.localToAncestor( scaledWorld, tool );
+				//				Rectangle worldBounds = FxUtil.toRectangle( worldToTool );
+				//				worldBounds.setFill( Color.TRANSPARENT );
+				//				worldBounds.setStroke( Color.YELLOW );
+				//				tool.getChildren().add( worldBounds );
+				//				System.out.println( "World to tool: " + worldToTool );
+
+				Bounds line1ToTool = FxUtil.localToAncestor( scaledLine1, tool );
+				Rectangle line1Bounds = FxUtil.toRectangle( line1ToTool );
+				line1Bounds.setFill( Color.TRANSPARENT );
+				line1Bounds.setStroke( Color.YELLOW );
+				tool.getChildren().add( line1Bounds );
+				System.out.println( "Line 1 to tool: " + line1ToTool );
+			} );
+		} ).start();
+	}
+
+	private void printBounds() {
+		System.out.println( "Stage size: " + stage.getWidth() + ", " + stage.getHeight() );
+		System.out.println( "Scene size: " + scene.getWidth() + ", " + scene.getHeight() );
+		System.out.println( "Tool size:  " + tool.getWidth() + ", " + tool.getHeight() );
+		System.out.println();
 	}
 
 }
