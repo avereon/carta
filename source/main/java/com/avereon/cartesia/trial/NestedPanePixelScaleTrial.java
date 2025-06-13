60f9965e983077809e3c44099dd312ffddf48d6d
msoderquist <msoderquist@mastercontrol.com>
2025-06-13 12:55:25 -0600

    Refactor NestedPanePixelScaleTrial to remove unused panes and adjust line scaling

--- source/main/java/com/avereon/cartesia/trial/NestedPanePixelScaleTrial.java
+++ source/main/java/com/avereon/cartesia/trial/NestedPanePixelScaleTrial.java
@@ -21,11 +21,7 @@ public class NestedPanePixelScaleTrial extends Application {
 
 	private Pane tool;
 
-	private Pane glass;
-
 	private Pane world;
-
-	private Pane scaledWorld;
 
 	@Override
 	public void start( Stage stage ) throws Exception {
@@ -40,51 +36,29 @@ public class NestedPanePixelScaleTrial extends Application {
 		// 2. Scale according to the desired design scale
 		// 3. Flip the Y-axis. In theory, this should be done in step 2 also.
 
-		// The unscaled world
-		Line line1 = new Line( -2, -2, 2, 2 );
+		// But because JavaFX works the way it does, have to pre-scale the geometry
+
+		// The pre-scaled world
+		Line line1 = new Line( -2 * scale, -2 * scale, 2 * scale, 2 * scale );
 		line1.setStroke( javafx.scene.paint.Color.RED.darker().darker() );
-		line1.setStrokeWidth( 1 );
+		line1.setStrokeWidth( 1 * scale );
 		line1.setStrokeLineCap( StrokeLineCap.ROUND );
-		Line line2 = new Line( -2, 2, 2, -2 );
+		Line line2 = new Line( -2 * scale, 2 * scale, 2 * scale, -2 * scale );
 		line2.setStroke( javafx.scene.paint.Color.GREEN );
-		line2.setStrokeWidth( 1 );
+		line2.setStrokeWidth( 1 * scale );
 		line2.setStrokeLineCap( StrokeLineCap.ROUND );
 
 		this.world = new Pane();
+		world.setScaleY( -1 );
 		world.getChildren().addAll( line1, line2 );
-		//world.resizeRelocate( 0, 0, 0,0 );
-
-		// The scaled world
-		Line scaledLine1 = new Line( -2 * scale, -2 * scale, 2 * scale, 2 * scale );
-		scaledLine1.setStroke( javafx.scene.paint.Color.RED.darker().darker() );
-		scaledLine1.setStrokeWidth( 1 * scale );
-		scaledLine1.setStrokeLineCap( StrokeLineCap.ROUND );
-		Line scaledLine2 = new Line( -2 * scale, 2 * scale, 2 * scale, -2 * scale );
-		scaledLine2.setStroke( javafx.scene.paint.Color.GREEN );
-		scaledLine2.setStrokeWidth( 1 * scale );
-		scaledLine2.setStrokeLineCap( StrokeLineCap.ROUND );
-
-		this.scaledWorld = new Pane();
-		scaledWorld.getChildren().addAll( scaledLine1, scaledLine2 );
-		//scaledWorld.resizeRelocate( 0, 0, 0,0 );
-//		scaledWorld.setTranslateX( 1 * scale );
-//		scaledWorld.setTranslateY( 1 * scale );
-
-		// The magnifying glass pane (translation, zoom, rotate)
-		this.glass = new Pane();
-		glass.relocate( 0.5 * width+ 200, 0.5 * height + 200);
-//		glass.setTranslateX( 1 );
-//		glass.setTranslateY( 1 );
-		glass.setScaleX( scale );
-		glass.setScaleY( scale );
-		glass.getChildren().add( world );
+		world.resizeRelocate( 0.5 * width, 0.5 * height - FxUtil.getContentBounds( world ).getHeight(), 0, 0 );
 
 		this.tool = new Pane();
 		tool.setStyle( "-fx-background-color: #222222;" );
 		Rectangle border = new Rectangle( 0.5, 0.5, width, height );
 		border.setFill( Color.TRANSPARENT );
 		border.setStroke( Color.GRAY );
-		tool.getChildren().addAll( border, glass );
+		tool.getChildren().addAll( border, world );
 
 		this.scene = new Scene( tool, width, height );
 		stage.setScene( scene );
@@ -92,6 +66,7 @@ public class NestedPanePixelScaleTrial extends Application {
 		stage.setTitle( "Nested Pane Pixel Scale Trial" );
 		stage.centerOnScreen();
 		stage.show();
+
 		// Apparently trying to set the stage size is inconsistent on Mint with my settings
 		// Stop fighting with it and move on
 		// Stage.sizeToScene can be inconsistent, so set the size directly
@@ -121,12 +96,12 @@ public class NestedPanePixelScaleTrial extends Application {
 				//				tool.getChildren().add( worldBounds );
 				//				System.out.println( "World to tool: " + worldToTool );
 
-				Bounds line1ToTool = FxUtil.localToAncestor( scaledLine1, tool );
-				Rectangle line1Bounds = FxUtil.toRectangle( line1ToTool );
+				Bounds line1BoundsInTool = FxUtil.localToAncestor( line1, tool );
+				Rectangle line1Bounds = FxUtil.toRectangle( line1BoundsInTool );
 				line1Bounds.setFill( Color.TRANSPARENT );
 				line1Bounds.setStroke( Color.YELLOW );
 				tool.getChildren().add( line1Bounds );
-				System.out.println( "Line 1 to tool: " + line1ToTool );
+				System.out.println( "Line 1 to tool: " + line1BoundsInTool );
 			} );
 		} ).start();
 	}
