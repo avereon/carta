4c54443f23a29dbf6c6f15eb3c6b6f1d2572166b
Mark Soderquist <mark@soderquist.net>
2025-07-15 11:56:56 -0600

    Revert "- Replace `EXTRA_LOOSE_TOLERANCE` with `TOLERANCE_PERCENT_LOOSE` for precision improvement."
    This reverts commit 519f5f9ac093f62af3c4591a8c26f0ebfec2b6f5.

--- source/test/java/com/avereon/cartesia/test/FxBoundsAssert.java
+++ source/test/java/com/avereon/cartesia/test/FxBoundsAssert.java
@@ -4,7 +4,6 @@ import javafx.geometry.Bounds;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.data.Offset;
-import org.assertj.core.data.Percentage;
 
 public class FxBoundsAssert extends AbstractAssert<FxBoundsAssert, Bounds> {
 
@@ -31,18 +30,6 @@ public class FxBoundsAssert extends AbstractAssert<FxBoundsAssert, Bounds> {
 		Assertions.assertThat( actual.getMaxX() ).isEqualTo( bounds.getMaxX(), offset );
 		Assertions.assertThat( actual.getMaxY() ).isEqualTo( bounds.getMaxY(), offset );
 		Assertions.assertThat( actual.getMaxZ() ).isEqualTo( bounds.getMaxZ(), offset );
-		return this;
-	}
-
-	public FxBoundsAssert isCloseTo( Bounds bounds, Percentage percentage ) {
-		Assertions.assertThat( actual.getMinX() ).isCloseTo( bounds.getMinX(), percentage );
-		Assertions.assertThat( actual.getMinY() ).isCloseTo( bounds.getMinY(), percentage );
-		Assertions.assertThat( actual.getMinZ() ).isCloseTo( bounds.getMinZ(), percentage );
-		Assertions.assertThat( actual.getWidth() ).isCloseTo( bounds.getWidth(), percentage );
-		Assertions.assertThat( actual.getHeight() ).isCloseTo( bounds.getHeight(), percentage );
-		Assertions.assertThat( actual.getMaxX() ).isCloseTo( bounds.getMaxX(), percentage );
-		Assertions.assertThat( actual.getMaxY() ).isCloseTo( bounds.getMaxY(), percentage );
-		Assertions.assertThat( actual.getMaxZ() ).isCloseTo( bounds.getMaxZ(), percentage );
 		return this;
 	}
 
