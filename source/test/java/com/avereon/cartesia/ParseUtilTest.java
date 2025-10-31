bf8cae63b2e8b08e5f57ddcbd45cb46a2c31e542
Mark Soderquist <mark@soderquist.net>
2025-10-31 12:42:34 -0600

    Add `parseValue`, refactor parsing utilities, and extend test coverage.
    - Introduced `parseValue` method for parsing single double values with improved validation logic.
    - Refactored `ParseUtil` to use non-null annotations and enhance code consistency.
    - Expanded unit tests to validate `parseValue`, `parsePoint2D`, and `parsePoint3D` with additional scenarios.

--- source/test/java/com/avereon/cartesia/ParseUtilTest.java
+++ source/test/java/com/avereon/cartesia/ParseUtilTest.java
@@ -1,5 +1,6 @@
 package com.avereon.cartesia;
 
+import javafx.geometry.Point2D;
 import javafx.geometry.Point3D;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -14,30 +15,87 @@ public class ParseUtilTest {
 
 	@ParameterizedTest
 	@MethodSource
-	public void testParsePoint3D( String string, Point3D expected, Exception exception ) {
+	void testParseValue( String string, Double expected, Exception exception ) {
+		if( exception == null ) {
+			assertThat( ParseUtil.parseValue( string ) ).isEqualTo( expected );
+		} else {
+			assertThatThrownBy( () -> ParseUtil.parseValue( string ) ).hasMessage( exception.getMessage() ).isInstanceOf( exception.getClass() );
+		}
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	void testParsePoint2D( String string, Point2D expected, Exception exception ) {
+		if( exception == null ) {
+			assertThat( ParseUtil.parsePoint2D( string ) ).isEqualTo( expected );
+		} else {
+			assertThatThrownBy( () -> ParseUtil.parsePoint2D( string ) ).hasMessage( exception.getMessage() ).isInstanceOf( exception.getClass() );
+		}
+	}
+
+	@ParameterizedTest
+	@MethodSource
+	void testParsePoint3D( String string, Point3D expected, Exception exception ) {
 		if( exception == null ) {
 			assertThat( ParseUtil.parsePoint3D( string ) ).isEqualTo( expected );
 		} else {
 			assertThatThrownBy( () -> ParseUtil.parsePoint3D( string ) ).hasMessage( exception.getMessage() ).isInstanceOf( exception.getClass() );
 		}
+	}
 
+	private static Stream<Arguments> testParseValue() {
+		return Stream.of(
+			// Valid Points
+			Arguments.of( "", 0.0, null ),
+			Arguments.of( " ", 0.0, null ),
+			Arguments.of( "0", 0.0, null ),
+			Arguments.of( "6", 6.0, null ),
+			Arguments.of( "-3.2e-17", -3.2e-17, null ),
+			Arguments.of( "-42.3", -42.3, null ),
+			Arguments.of( "\t-42.3\n", -42.3, null ),
+			// More than necessary, but still valid
+			Arguments.of( "0,0", 0.0, null ),
+			Arguments.of( "0,0,0", 0.0, null ),
+			Arguments.of( "0,0,0,0", 0.0, null ),
+			Arguments.of( "0,0,0,0,0", 0.0, null ),
+			// Exceptions
+			Arguments.of( "Not a Value", null, new NumberFormatException( "For input string: \"Not a Value\"" ) )
+		);
+	}
+
+	private static Stream<Arguments> testParsePoint2D() {
+		return Stream.of(
+			// Valid Points
+			Arguments.of( "", Point2D.ZERO, null ),
+			Arguments.of( " ", Point2D.ZERO, null ),
+			Arguments.of( "0", Point2D.ZERO, null ),
+			Arguments.of( "0,0", Point2D.ZERO, null ),
+			Arguments.of( "-3e-17,-42.3", new Point2D( -3e-17, -42.3 ), null ),
+			Arguments.of( " -3.2e-17 , -42.3 ", new Point2D( -3.2e-17, -42.3 ), null ),
+			// More than necessary, but still valid
+			Arguments.of( "0,0,0", Point2D.ZERO, null ),
+			Arguments.of( "0,0,0,0", Point2D.ZERO, null ),
+			Arguments.of( "1,2,3,4,5", new Point2D( 1, 2 ), null ),
+			// Exceptions
+			Arguments.of( "Not,a,Point", null, new NumberFormatException( "For input string: \"Not\"" ) )
+		);
 	}
 
 	private static Stream<Arguments> testParsePoint3D() {
 		return Stream.of(
-			// Exceptions
-			Arguments.of( null, null, new NullPointerException( "Point string cannot be null" ) ),
-			Arguments.of( "", null, new IllegalArgumentException( "Input string cannot be blank" ) ),
-			Arguments.of( "  ", null, new IllegalArgumentException( "Input string cannot be blank" ) ),
-			Arguments.of( "Not,a,Point", null, new NumberFormatException( "For input string: \"Not\"" ) ),
 			// Valid Points
+			Arguments.of( "", Point3D.ZERO, null ),
+			Arguments.of( " ", Point3D.ZERO, null ),
 			Arguments.of( "0", Point3D.ZERO, null ),
 			Arguments.of( "0,0", Point3D.ZERO, null ),
 			Arguments.of( "0,0,0", Point3D.ZERO, null ),
-			Arguments.of( "6,-3e-17,-42.3", new Point3D( 6, -3e-17, -42.3 ), null ),
+			Arguments.of( "6,-3.2e-17,-42.3", new Point3D( 6, -3.2e-17, -42.3 ), null ),
+			Arguments.of( " 6 , -3.2e-17 , -42.3 ", new Point3D( 6, -3.2e-17, -42.3 ), null ),
 			// More than necessary, but still valid
 			Arguments.of( "0,0,0,0", Point3D.ZERO, null ),
-			Arguments.of( "0,0,0,0,0", Point3D.ZERO, null )
+			Arguments.of( "1,2,3,4,5", new Point3D( 1, 2, 3 ), null ),
+			// Exceptions
+			Arguments.of( "Not,a,Point", null, new NumberFormatException( "For input string: \"Not\"" ) )
 		);
 	}
 
